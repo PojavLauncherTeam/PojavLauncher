cc005abb991ad53479a00922331984128f91f9d8
khanhduytran0 <khanhduytran032@gmail.com>
2020-04-20 13:00:52 +0700

    Fix attribute

--- app/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
+++ app/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
@@ -3,6 +3,7 @@ package net.kdt.pojavlaunch.prefs;
 import android.os.*;
 import net.kdt.pojavlaunch.*;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SeekBarPreference;
 
 public class LauncherPreferenceFragment extends PreferenceFragmentCompat
 {
@@ -11,6 +12,16 @@ public class LauncherPreferenceFragment extends PreferenceFragmentCompat
 		addPreferencesFromResource(R.xml.pref_main);
 		
 		// Disable freeform mode in Android 6.0 or below.
-		getPreferenceScreen().findPreference("freeform").setEnabled(Build.VERSION.SDK_INT >= 24);
+		findPreference("freeform").setEnabled(Build.VERSION.SDK_INT >= 24);
+		
+		SeekBarPreference seek1 = (SeekBarPreference) findPreference("maxDxRefs");
+		seek1.setMin(0xFFF);
+		seek1.setMax(0xFFFF);
+		seek1.setValue(0xFFF);
+		
+		SeekBarPreference seek2 = (SeekBarPreference) findPreference("timeLongPressTrigger");
+		seek2.setMin(100);
+		seek2.setMax(1000);
+		seek2.setValue(500);
 	}
 }
