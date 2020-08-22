320c7af46c2f89dcc7ef8ef01436b421b2a53576
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-22 16:07:23 +0700

    More cleanup and additional

---
+++ app/src/main/java/net/kdt/pojavlaunch/BinaryExecutor.java
@@ -0,0 +1,11 @@
+package net.kdt.pojavlaunch;
+
+// This class load and execute PIE binary using dlopen and dlsym("main")
+public class BinaryExecutor
+{
+	public static native int executeBinary(String[] args);
+	
+	static {
+		System.loadLibrary("binexecutor");
+	}
+}
