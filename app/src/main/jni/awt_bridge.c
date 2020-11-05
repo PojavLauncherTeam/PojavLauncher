292fe4bca5ecf92584ea1849ace73027dcc74150
khanhduytran0 <khanhduytran032@gmail.com>
2020-11-05 10:07:48 +0700

    [Mod installer] Fix wrong JNIEnv passed to parts

--- app/src/main/jni/awt_bridge.c
+++ app/src/main/jni/awt_bridge.c
@@ -22,14 +22,14 @@ JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_JREUtils_renderAWTScreenFrame(JN
     // Copy JRE RGB array memory to Android.
     int arrayLength = (*runtimeJNIEnvPtr_ANDROID)->GetArrayLength(runtimeJNIEnvPtr_ANDROID, jreRgbArray);
     rgbArray = (*runtimeJNIEnvPtr_ANDROID)->GetIntArrayElements(runtimeJNIEnvPtr_ANDROID, jreRgbArray, 0);
-    androidRgbArray = (*runtimeJNIEnvPtr_ANDROID)->NewIntArray(runtimeJNIEnvPtr_ANDROID, arrayLength);
-    (*runtimeJNIEnvPtr_ANDROID)->SetIntArrayRegion(runtimeJNIEnvPtr_ANDROID, androidRgbArray, 0, arrayLength, rgbArray);
+    androidRgbArray = (*dalvikJNIEnvPtr_ANDROID)->NewIntArray(dalvikJNIEnvPtr_ANDROID, arrayLength);
+    (*dalvikJNIEnvPtr_ANDROID)->SetIntArrayRegion(dalvikJNIEnvPtr_ANDROID, androidRgbArray, 0, arrayLength, rgbArray);
     
     // Maybe use Skia lib instead?
     jclass class_canvas = (*dalvikJNIEnvPtr_ANDROID)->GetObjectClass(dalvikJNIEnvPtr_ANDROID, canvas);
     jmethodID method_canvas = (*dalvikJNIEnvPtr_ANDROID)->GetMethodID(dalvikJNIEnvPtr_ANDROID, class_canvas, "drawBitmap", "([IIIFFIIZLandroid/graphics/Paint;)V");
-    (*runtimeJNIEnvPtr_ANDROID)->CallVoidMethod(
-        runtimeJNIEnvPtr_ANDROID,
+    (*dalvikJNIEnvPtr_ANDROID)->CallVoidMethod(
+        dalvikJNIEnvPtr_ANDROID,
         canvas,
         method_canvas,
         rgbArray, 0, width, 0, 0, width, height, JNI_TRUE, NULL
