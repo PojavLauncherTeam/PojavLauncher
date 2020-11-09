5f12dc0dcd592e677774f6a6ef6d0eb87842dd4c
khanhduytran0 <khanhduytran032@gmail.com>
2020-11-09 16:38:48 +0700

    [libawt_xawt] Implement missing X11SurfaceData symbols

--- app/src/main/jni/awt_xawt/xawt_fake.c
+++ app/src/main/jni/awt_xawt/xawt_fake.c
@@ -31,5 +31,18 @@ JNIEXPORT void JNICALL Java_java_awt_KeyboardFocusManager_initIDs(JNIEnv *env, j
 JNIEXPORT void JNICALL Java_java_awt_TrayIcon_initIDs(JNIEnv *env, jclass cls) {}
 JNIEXPORT void JNICALL Java_sun_awt_X11_XWindow_initIDs(JNIEnv *env, jclass cls) {}
 JNIEXPORT void JNICALL Java_sun_font_SunFontManager_initIDs(JNIEnv *env, jclass cls) {}
+
 JNIEXPORT void JNICALL Java_sun_java2d_x11_X11SurfaceData_initIDs(JNIEnv *env, jclass cls) {}
+JNIEXPORT void JNICALL Java_sun_java2d_x11_X11SurfaceData_initSurface(JNIEnv *env, jclass cls, jint depth, jint width, jint height, jlong drawable) {
+    // Any ideas to implement?
+}
+JNIEXPORT jboolean JNICALL Java_sun_java2d_x11_X11SurfaceData_isDgaAvailable(JNIEnv *env, jclass cls) {
+    return JNI_FALSE;
+}
+JNIEXPORT jboolean JNICALL Java_sun_java2d_x11_X11SurfaceData_isShmPMAvailable(JNIEnv *env, jclass cls) {
+    return JNI_FALSE;
+}
+JNIEXPORT void JNICALL Java_sun_java2d_x11_X11SurfaceData_XSetCopyMode(JNIEnv *env, jclass cls, jlong xgc) {}
+JNIEXPORT void JNICALL Java_sun_java2d_x11_X11SurfaceData_XSetXorMode(JNIEnv *env, jclass cls, jlong xgv) {}
+JNIEXPORT void JNICALL Java_sun_java2d_x11_X11SurfaceData_XSetForeground(JNIEnv *env, jclass cls, jlong xgc, jint pixel) {}
 
