90078cf1c91f836aa1644f4542e7d726acb15d65
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-11-19 09:59:19 +0700

    Update languagelist_updater.sh

--- scripts/languagelist_updater.sh
+++ scripts/languagelist_updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-THISDIR = `dirname $0`
-LANGFILE = $THISDIR/../app/src/main/assets/language_list.txt
+THISDIR=`dirname $0`
+LANGFILE=$THISDIR/../app/src/main/assets/language_list.txt
 
 rm $LANGFILE
 ls $THISDIR/../app/src/main/res/values-* >> $LANGFILE
