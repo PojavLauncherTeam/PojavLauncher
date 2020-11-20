c25b7bd7688e40fba82a823cc5dc453da1f34416
khanhduytran0 <khanhduytran032@gmail.com>
2020-11-21 06:16:54 +0700

    Add LIBGL_NORMALIZE=1 to fix banner and sheep color

--- app/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -191,9 +191,14 @@ public class JREUtils
         envMap.put("HOME", Tools.MAIN_PATH);
         envMap.put("TMPDIR", ctx.getCacheDir().getAbsolutePath());
         envMap.put("LIBGL_MIPMAP", "3");
+        
+        // Fix white color on banner and sheep
+        envMap.put("LIBGL_NORMALIZE", "1");
+        
    // those 2 down below make it more laggy also kinda usless as they dont fix the sheep collor \\
    //     envMap.put("LIBGL_BATCH", "10");
    //     envMap.put("LIBGL_USEVBO", "0");
+   
         envMap.put("MESA_GLSL_CACHE_DIR", ctx.getCacheDir().getAbsolutePath());
         envMap.put("LD_LIBRARY_PATH", LD_LIBRARY_PATH);
         envMap.put("PATH", Tools.homeJreDir + "/bin:" + Os.getenv("PATH"));
