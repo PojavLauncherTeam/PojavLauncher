32bf68bdb7d7e50b9dd8588e0793aea83740b436
khanhduytran0 <khanhduytran032@gmail.com>
2020-11-25 19:46:37 +0700

    [Input pipe] Try to fix double keys without trigger backspace

--- app/src/main/jni/input_bridge_v3.c
+++ app/src/main/jni/input_bridge_v3.c
@@ -278,10 +278,10 @@ JNIEXPORT void JNICALL Java_org_lwjgl_glfw_CallbackBridge_nativeSendKey(JNIEnv* 
 
 JNIEXPORT void JNICALL Java_org_lwjgl_glfw_CallbackBridge_nativeSendKeycode(JNIEnv* env, jclass clazz, jint keycode, jchar keychar, jint scancode, jint action, jint mods) {
     if (isInputReady) {
+        Java_org_lwjgl_glfw_CallbackBridge_nativeSendKey(env, clazz, keycode, scancode, action, mods);
         if (!Java_org_lwjgl_glfw_CallbackBridge_nativeSendCharMods(env, clazz, keychar, mods)) {
             Java_org_lwjgl_glfw_CallbackBridge_nativeSendChar(env, clazz, keychar);
         }
-        Java_org_lwjgl_glfw_CallbackBridge_nativeSendKey(env, clazz, keycode, scancode, action, mods);
     }
 }
 
