bc5f94e2ee5794981c5d9b3c9635d20ae36e5699
khanhduytran0 <khanhduytran032@gmail.com>
2020-12-09 13:05:07 +0700

    [MS OAuth] Try to return back to activity after login

--- app/src/main/java/net/kdt/pojavlaunch/CustomTabs.java
+++ app/src/main/java/net/kdt/pojavlaunch/CustomTabs.java
@@ -1,13 +1,9 @@
 package net.kdt.pojavlaunch;
 
-import android.app.*;
 import android.content.*;
-import android.graphics.*;
 import android.net.*;
-import android.support.customtabs.*;
-import android.support.v4.content.*;
 import android.widget.*;
-import net.kdt.pojavlaunch.*;
+import androidx.browser.customtabs.*;
 
 public class CustomTabs {
 
@@ -17,19 +13,5 @@ public class CustomTabs {
         
         CustomTabsIntent customTabsIntent = builder.build();
         customTabsIntent.launchUrl(context, Uri.parse(url));
-    }
-
-    public static class CopyBroadcastReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String url = intent.getDataString();
-
-            ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData data = ClipData.newPlainText("Link", url);
-            clipboardManager.setPrimaryClip(data);
-
-            Toast.makeText(context, "Copied " + url, Toast.LENGTH_SHORT).show();
-        }
     }
 }
