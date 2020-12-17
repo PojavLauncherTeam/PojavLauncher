64624531b47cac66b7d1a6b55ab4a7acde7e7267
khanhduytran0 <khanhduytran032@gmail.com>
2020-12-17 20:27:18 +0700

    [Probably should not try] Changes - Bug fix on migrate to new game dir - Update sandbox policies

--- app/src/main/java/net/kdt/pojavlaunch/PojavMigrator.java
+++ app/src/main/java/net/kdt/pojavlaunch/PojavMigrator.java
@@ -7,7 +7,6 @@ public class PojavMigrator
 {
     public static void migrateAccountData(Context ctx) {
         File oldAccDir = new File(Tools.DIR_ACCOUNT_OLD);
-        
         if (oldAccDir.exists() && oldAccDir.isDirectory()) {
             for (String account : oldAccDir.list()) {
                 File oldAccFile = new File(oldAccDir, account);
@@ -32,16 +31,42 @@ public class PojavMigrator
         }
     }
     
-    public static void migrateGameDir() throws IOException, InterruptedException {
+    public static boolean migrateGameDir() throws IOException, InterruptedException {
         File oldGameDir = new File(Tools.DIR_GAME_OLD);
-        if (oldGameDir.exists() && oldGameDir.isDirectory()) {
-            Process p = Runtime.getRuntime().exec(
-                new String[]{"mv", Tools.DIR_GAME_OLD, Tools.DIR_GAME_NEW});
-            int exitCode = p.waitFor();
-            if (exitCode != 0) {
-                throw new IOException("Could not move game dir! Exit code " + exitCode +
-                    ", message:\n" + Tools.read(p.getErrorStream()));
-            }
+        
+        boolean moved = oldGameDir.exists() && oldGameDir.isDirectory();
+        if (!migrateBugFix20201217() && moved) {
+            command("mv " + Tools.DIR_GAME_OLD + " " + Tools.DIR_GAME_HOME + "/");
+        }
+
+        return moved;
+    }
+    
+    public static boolean migrateBugFix20201217() throws IOException, InterruptedException {
+        File bugGameDir = new File(Tools.DIR_GAME_NEW + "/.minecraft");
+        File oldGameDir = new File(Tools.DIR_GAME_OLD);
+        boolean moved = bugGameDir.exists() && bugGameDir.isDirectory();
+        
+        if (oldGameDir.exists() && oldGameDir.isDirectory() && moved) {
+            command("rm -rf " + oldGameDir.getAbsolutePath());
+        }
+        
+        if (moved) {
+            command("mv " + bugGameDir.getAbsolutePath() + " " + Tools.DIR_GAME_OLD);
+            command("rm -rf " + Tools.DIR_GAME_HOME);
+            command("mv " + Tools.DIR_GAME_OLD + " " + Tools.DIR_GAME_HOME + "/");
+        }
+
+        return moved;
+    }
+    
+    private static void command(String cmd) throws IOException, InterruptedException {
+        Process p = Runtime.getRuntime().exec(
+            new String[]{cmd});
+        int exitCode = p.waitFor();
+        if (exitCode != 0) {
+            throw new IOException("Exit code " + exitCode +
+                                  ", message:\n" + Tools.read(p.getErrorStream()));
         }
     }
 }
