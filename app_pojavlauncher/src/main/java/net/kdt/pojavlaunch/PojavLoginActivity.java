5f2e5c1079e7779fbe2c411749d944dca8f8cc3f
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-01-17 09:49:16 +0700

    Update PojavLoginActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
@@ -773,6 +773,10 @@ public class PojavLoginActivity extends BaseActivity
                     }
                 }
             });
+
+            // Tiny trick to avoid 'const' field
+            final int accountIndex_final = accountIndex;
+
             removeButton.setOnClickListener(new View.OnClickListener() {
                 final String selectedAccName = accountName.getText().toString();
                 @Override
@@ -785,7 +789,7 @@ public class PojavLoginActivity extends BaseActivity
                         @Override
                         public void onClick(DialogInterface p1, int p2) {
                             new InvalidateTokenTask(PojavLoginActivity.this).execute(selectedAccName);
-                            accountListLayout.removeViewsInLayout(accountIndex, 1);
+                            accountListLayout.removeViewsInLayout(accountIndex_final, 1);
                             //Resize the window
                             accountDialog.getWindow().setLayout((int)(xScreen*0.4),(int) Math.min((yScreen*0.8), (73 + accountListLayout.getChildCount()*55)*(PojavLoginActivity.this.getResources().getDisplayMetrics().densityDpi/160f) ));
                         }
