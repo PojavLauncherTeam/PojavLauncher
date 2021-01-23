96b267f8678b37e4ad54c50cfdcf10d299850c5d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-01-24 04:57:09 +0700

    [JVM args editor] use java8 syntax

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
@@ -9,22 +9,22 @@ import android.content.*;
 
 public class LauncherPreferenceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener
 {
-	@Override
-	public void onCreatePreferences(Bundle b, String str) {
-		addPreferencesFromResource(R.xml.pref_main);
+    @Override
+    public void onCreatePreferences(Bundle b, String str) {
+        addPreferencesFromResource(R.xml.pref_main);
 		
-		// Disable freeform mode in Android 6.0 and below.
-		findPreference("freeform").setEnabled(Build.VERSION.SDK_INT >= 24);
+        // Disable freeform mode in Android 6.0 and below.
+        findPreference("freeform").setEnabled(Build.VERSION.SDK_INT >= 24);
 		
-		SeekBarPreference seek2 = (SeekBarPreference) findPreference("timeLongPressTrigger");
-		seek2.setMin(100);
-		seek2.setMax(1000);
-		seek2.setValue(LauncherPreferences.PREF_LONGPRESS_TRIGGER);
+        SeekBarPreference seek2 = (SeekBarPreference) findPreference("timeLongPressTrigger");
+        seek2.setMin(100);
+        seek2.setMax(1000);
+        seek2.setValue(LauncherPreferences.PREF_LONGPRESS_TRIGGER);
         
         SeekBarPreference seek3 = (SeekBarPreference) findPreference("buttonscale");
         seek3.setMin(20);
         seek3.setMax(500);
-		seek3.setValue((int) LauncherPreferences.PREF_BUTTONSIZE);
+        seek3.setValue((int) LauncherPreferences.PREF_BUTTONSIZE);
         
         SeekBarPreference seek4 = (SeekBarPreference) findPreference("mousescale");
         seek4.setMin(20);
@@ -33,14 +33,9 @@ public class LauncherPreferenceFragment extends PreferenceFragmentCompat impleme
         
         EditTextPreference editJVMArgs = findPreference("javaArgs");
         if (editJVMArgs != null) {
-            editJVMArgs.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
-                @Override
-                public void onBindEditText(@NonNull EditText editText) {
-                    editText.setSingleLine();
-                }
-            });
+            editJVMArgs.setOnBindEditTextListener((editText) -> editText.setSingleLine());
         }
-	}
+    }
 
     @Override
     public void onResume() {
