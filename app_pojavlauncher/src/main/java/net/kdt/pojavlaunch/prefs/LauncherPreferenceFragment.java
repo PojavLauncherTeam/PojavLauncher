ec668827e033592b6e16bedc905643a0548bc0fd
SerpentSpirale <serpentspirale@gmail.com>
2021-01-26 11:19:49 +0100

    - Make use of the CustomSeekBarPreference. - Tweaked seekbar values range to be more reasonnable.

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferenceFragment.java
@@ -1,7 +1,7 @@
 package net.kdt.pojavlaunch.prefs;
 
 import android.os.*;
-import androidx.core.app.*;
+
 import androidx.preference.*;
 
 import net.kdt.pojavlaunch.R;
@@ -16,20 +16,27 @@ public class LauncherPreferenceFragment extends PreferenceFragmentCompat impleme
         // Disable freeform mode in Android 6.0 and below.
         findPreference("freeform").setEnabled(Build.VERSION.SDK_INT >= 24);
 		
-        SeekBarPreference seek2 = (SeekBarPreference) findPreference("timeLongPressTrigger");
+        CustomSeekBarPreference seek2 = (CustomSeekBarPreference) findPreference("timeLongPressTrigger");
         seek2.setMin(100);
         seek2.setMax(1000);
         seek2.setValue(LauncherPreferences.PREF_LONGPRESS_TRIGGER);
+        seek2.setSuffix(" ms");
         
-        SeekBarPreference seek3 = (SeekBarPreference) findPreference("buttonscale");
-        seek3.setMin(20);
-        seek3.setMax(500);
+        CustomSeekBarPreference seek3 = (CustomSeekBarPreference) findPreference("buttonscale");
+        seek3.setMin(80);
+        seek3.setMax(250);
         seek3.setValue((int) LauncherPreferences.PREF_BUTTONSIZE);
-        
-        SeekBarPreference seek4 = (SeekBarPreference) findPreference("mousescale");
-        seek4.setMin(20);
-        seek4.setMax(500);
+        seek3.setSuffix(" %");
+
+        CustomSeekBarPreference seek4 = (CustomSeekBarPreference) findPreference("mousescale");
+        seek4.setMin(100);
+        seek4.setMax(300);
         seek4.setValue((int) LauncherPreferences.PREF_MOUSESCALE);
+        seek4.setSuffix(" %");
+
+        CustomSeekBarPreference seek5 = (CustomSeekBarPreference) findPreference("resolutionRatio");
+        seek5.setMin(25);
+        seek5.setSuffix(" %");
         
         EditTextPreference editJVMArgs = findPreference("javaArgs");
         if (editJVMArgs != null) {
