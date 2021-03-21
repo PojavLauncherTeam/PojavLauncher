4ed4405588b1b8745343dc2cf00066118f831cb0
Campbell Suter <znix@znix.xyz>
2021-03-21 19:51:39 +1300

    Add the hacky and very-reliant-on-external-setup vivecraft startup stuff

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/QuestMainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/QuestMainActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.app.NativeActivity;
 import android.util.Log;
 
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 import net.kdt.pojavlaunch.utils.JREUtils;
+import net.kdt.pojavlaunch.value.DependentLibrary;
+import net.kdt.pojavlaunch.value.MinecraftAccount;
+import net.kdt.pojavlaunch.value.MinecraftLibraryArtifact;
 
 import org.apache.commons.io.IOUtils;
 
@@ -16,6 +20,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuestMainActivity extends NativeActivity implements ILoggableActivity {
     private static final String TAG = "QuestActivity";
@@ -60,11 +65,34 @@ public class QuestMainActivity extends NativeActivity implements ILoggableActivi
     public String[] getJavaArgs() {
         ArrayList<String> args = new ArrayList<>();
         args.add("java");
-        args.add("-cp");
-        args.add(Tools.getLWJGL3ClassPath() + ":" + APP_JAR);
         Tools.getJavaArgs(this, args);
 
-        args.add("xyz.znix.graphicstest.Main");
+        if (true) {
+            String versionName = "vivecraft-1.12.2-jrbudda-11-r2";
+            // versionName = "1.12.2";
+            JMinecraftVersionList.Version versionInfo = Tools.getVersionInfo(null, versionName);
+            String launchClassPath = Tools.generateLaunchClassPath(versionInfo, versionName);
+
+            checkLibraries(versionInfo);
+
+            // HACK: Add in Vanilla since vivecraft doesn't include it
+            // launchClassPath += ":/sdcard/games/PojavLauncher/.minecraft/versions/1.12.2/1.12.2.jar";
+
+            args.add("-cp");
+            args.add(Tools.getLWJGL3ClassPath() + ":" + launchClassPath);
+
+            // args.add("net.minecraft.client.main.Main");
+            args.add(versionInfo.mainClass);
+
+            MinecraftAccount profile = MinecraftAccount.load("ZNixian");
+            String[] launchArgs = Tools.getMinecraftArgs(profile, versionInfo, Tools.DIR_GAME_NEW);
+            args.addAll(Arrays.asList(launchArgs));
+        } else {
+            args.add("-cp");
+            args.add(Tools.getLWJGL3ClassPath() + ":" + APP_JAR);
+
+            args.add("xyz.znix.graphicstest.Main");
+        }
 
         return args.toArray(new String[0]);
     }
@@ -77,5 +105,80 @@ public class QuestMainActivity extends NativeActivity implements ILoggableActivi
     @Override
     public void appendToLog(String text, boolean checkAllow) {
         Log.i(TAG, text);
+    }
+
+    private void checkLibraries(JMinecraftVersionList.Version version) {
+        // From MinecraftDownloaderTask
+        for (final DependentLibrary libItem : version.libraries) {
+
+            if (libItem.name.startsWith("net.java.jinput") || libItem.name.startsWith("org.lwjgl")) {
+                // ignore
+            } else {
+                String[] libInfo = libItem.name.split(":");
+                String libArtifact = Tools.artifactToPath(libInfo[0], libInfo[1], libInfo[2]);
+                File outLib = new File(Tools.DIR_HOME_LIBRARY + "/" + libArtifact);
+                outLib.getParentFile().mkdirs();
+
+                if (!outLib.exists()) {
+                    downloadLibrary(libItem, libArtifact, outLib);
+                    Log.w(TAG, "should download " + outLib);
+                }
+
+                // TODO hash check
+            }
+        }
+    }
+
+    protected void downloadLibrary(DependentLibrary libItem, String libArtifact, File outLib) {
+        // From MinecraftDownloaderTask
+
+        String libPathURL;
+
+        if (libItem.downloads == null || libItem.downloads.artifact == null) {
+            System.out.println("UnkLib:" + libArtifact);
+            MinecraftLibraryArtifact artifact = new MinecraftLibraryArtifact();
+
+            // HACK around Vivecraft URLs, HTTPS doesn't work for them
+            String server = libItem.url == null ? "https://libraries.minecraft.net/" : libItem.url;
+            if (!server.contains("vivecraft.org"))
+                server = server.replace("http://", "https://");
+
+            artifact.url = server + libArtifact;
+            libItem.downloads = new DependentLibrary.LibraryDownloads(artifact);
+        }
+
+        Log.i(TAG, "Download: " + libItem.downloads.artifact.url);
+
+        try {
+            libPathURL = libItem.downloads.artifact.url;
+            boolean isFileGood = false;
+            byte timesChecked = 0;
+            while (!isFileGood) {
+                timesChecked++;
+                if (timesChecked > 5)
+                    throw new RuntimeException("Library download failed after 5 retries");
+                Tools.downloadFileMonitored(
+                        libPathURL,
+                        outLib.getAbsolutePath(),
+                        new Tools.DownloaderFeedback() {
+                            @Override
+                            public void updateProgress(int curr, int max) {
+                            }
+                        }
+                );
+                if (libItem.downloads.artifact.sha1 != null) {
+                    isFileGood = !LauncherPreferences.PREF_CHECK_LIBRARY_SHA || Tools.compareSHA1(outLib, libItem.downloads.artifact.sha1);
+                    if (!isFileGood) {
+                        Log.e(TAG, "Bad hash for " + libItem.name);
+                        outLib.delete();
+                        return;
+                    }
+                } else {
+                    isFileGood = true;
+                }
+            }
+        } catch (Exception th) {
+            Log.e(TAG, "Failed downloading libraries", th);
+        }
     }
 }
