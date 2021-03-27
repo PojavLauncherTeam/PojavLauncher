636ca31e2cb048a694736165a5baa958074f2bef
artdeell <ultramaksim@gmail.com>
2021-03-27 21:33:51 +0300

    Automatic stride to un-broke the image

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -20,163 +20,6 @@
 #include <android/rect.h>
 #include <string.h>
 #include "utils.h"
-#define OSMESA_UNSAFE
-#ifdef OSMESA_UNSAFE
-typedef struct native_handle
-{
-    int version;        /* sizeof(native_handle_t) */
-    int numFds;         /* number of file-descriptors at &data[0] */
-    int numInts;        /* number of ints at &data[numFds] */
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wzero-length-array"
-#endif
-    int data[0];        /* numFds + numInts ints */
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#endif
-} native_handle_t;
-typedef const native_handle_t* buffer_handle_t;
-typedef struct android_native_base_t
-{
-    /* a magic value defined by the actual EGL native type */
-    int magic;
-    /* the sizeof() of the actual EGL native type */
-    int version;
-    void* reserved[4];
-    /* reference-counting interface */
-    void (*incRef)(struct android_native_base_t* base);
-    void (*decRef)(struct android_native_base_t* base);
-} android_native_base_t;
-typedef struct ANativeWindowBuffer
-{
-    struct android_native_base_t common;
-    int width;
-    int height;
-    int stride;
-    int format;
-    int usage;
-    void* reserved[2];
-    buffer_handle_t handle;
-    void* reserved_proc[8];
-} ANativeWindowBuffer_t;
-struct ANativeWindow_sys
-{
-    struct android_native_base_t common;
-    /* flags describing some attributes of this surface or its updater */
-    const uint32_t flags;
-    /* min swap interval supported by this updated */
-    const int   minSwapInterval;
-    /* max swap interval supported by this updated */
-    const int   maxSwapInterval;
-    /* horizontal and vertical resolution in DPI */
-    const float xdpi;
-    const float ydpi;
-    /* Some storage reserved for the OEM's driver. */
-    intptr_t    oem[4];
-    /*
-     * Set the swap interval for this surface.
-     *
-     * Returns 0 on success or -errno on error.
-     */
-    int     (*setSwapInterval)(struct ANativeWindow* window,
-                int interval);
-    /*
-     * Hook called by EGL to acquire a buffer. After this call, the buffer
-     * is not locked, so its content cannot be modified. This call may block if
-     * no buffers are available.
-     *
-     * The window holds a reference to the buffer between dequeueBuffer and
-     * either queueBuffer or cancelBuffer, so clients only need their own
-     * reference if they might use the buffer after queueing or canceling it.
-     * Holding a reference to a buffer after queueing or canceling it is only
-     * allowed if a specific buffer count has been set.
-     *
-     * Returns 0 on success or -errno on error.
-     */
-    int     (*dequeueBuffer)(struct ANativeWindow* window,
-                struct ANativeWindowBuffer** buffer);
-    /*
-     * hook called by EGL to lock a buffer. This MUST be called before modifying
-     * the content of a buffer. The buffer must have been acquired with
-     * dequeueBuffer first.
-     *
-     * Returns 0 on success or -errno on error.
-     */
-    int     (*lockBuffer)(struct ANativeWindow* window,
-                struct ANativeWindowBuffer* buffer);
-    /*
-     * Hook called by EGL when modifications to the render buffer are done.
-     * This unlocks and post the buffer.
-     *
-     * The window holds a reference to the buffer between dequeueBuffer and
-     * either queueBuffer or cancelBuffer, so clients only need their own
-     * reference if they might use the buffer after queueing or canceling it.
-     * Holding a reference to a buffer after queueing or canceling it is only
-     * allowed if a specific buffer count has been set.
-     *
-     * Buffers MUST be queued in the same order than they were dequeued.
-     *
-     * Returns 0 on success or -errno on error.
-     */
-    int     (*queueBuffer)(struct ANativeWindow* window,
-                struct ANativeWindowBuffer* buffer);
-    /*
-     * hook used to retrieve information about the native window.
-     *
-     * Returns 0 on success or -errno on error.
-     */
-    int     (*query)(const struct ANativeWindow* window,
-                int what, int* value);
-    /*
-     * hook used to perform various operations on the surface.
-     * (*perform)() is a generic mechanism to add functionality to
-     * ANativeWindow while keeping backward binary compatibility.
-     *
-     * DO NOT CALL THIS HOOK DIRECTLY.  Instead, use the helper functions
-     * defined below.
-     *
-     *  (*perform)() returns -ENOENT if the 'what' parameter is not supported
-     *  by the surface's implementation.
-     *
-     * The valid operations are:
-     *     NATIVE_WINDOW_SET_USAGE
-     *     NATIVE_WINDOW_CONNECT               (deprecated)
-     *     NATIVE_WINDOW_DISCONNECT            (deprecated)
-     *     NATIVE_WINDOW_SET_CROP
-     *     NATIVE_WINDOW_SET_BUFFER_COUNT
-     *     NATIVE_WINDOW_SET_BUFFERS_GEOMETRY  (deprecated)
-     *     NATIVE_WINDOW_SET_BUFFERS_TRANSFORM
-     *     NATIVE_WINDOW_SET_BUFFERS_TIMESTAMP
-     *     NATIVE_WINDOW_SET_BUFFERS_DIMENSIONS
-     *     NATIVE_WINDOW_SET_BUFFERS_FORMAT
-     *     NATIVE_WINDOW_SET_SCALING_MODE
-     *     NATIVE_WINDOW_LOCK                   (private)
-     *     NATIVE_WINDOW_UNLOCK_AND_POST        (private)
-     *     NATIVE_WINDOW_API_CONNECT            (private)
-     *     NATIVE_WINDOW_API_DISCONNECT         (private)
-     *
-     */
-    int     (*perform)(struct ANativeWindow* window,
-                int operation, ... );
-    /*
-     * Hook used to cancel a buffer that has been dequeued.
-     * No synchronization is performed between dequeue() and cancel(), so
-     * either external synchronization is needed, or these functions must be
-     * called from the same thread.
-     *
-     * The window holds a reference to the buffer between dequeueBuffer and
-     * either queueBuffer or cancelBuffer, so clients only need their own
-     * reference if they might use the buffer after queueing or canceling it.
-     * Holding a reference to a buffer after queueing or canceling it is only
-     * allowed if a specific buffer count has been set.
-     */
-    int     (*cancelBuffer)(struct ANativeWindow* window,
-                struct ANativeWindowBuffer* buffer);
-    void* reserved_proc[2];
-};
-typedef struct ANativeWindow_sys _ANativeWindow_sys2;
-#endif
 struct PotatoBridge {
 	 /*ANativeWindow */ void* androidWindow;
 	    
@@ -253,9 +96,7 @@ JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglInit(JNIEnv* env, j
     ANativeWindow_acquire(potatoBridge.androidWindow);
     width = ANativeWindow_getWidth(potatoBridge.androidWindow);
     height = ANativeWindow_getHeight(potatoBridge.androidWindow);
-    _ANativeWindow_sys2 window = *(_ANativeWindow_sys2*)potatoBridge.androidWindow;
-    printf("OSMDroid: internal dequeueBuffer ptr=%p",window.dequeueBuffer);
-    ANativeWindow_setBuffersGeometry(potatoBridge.androidWindow,width,height,AHARDWAREBUFFER_FORMAT_R8G8B8X8_UNORM);
+    ANativeWindow_setBuffersGeometry(potatoBridge.androidWindow,width,height,AHARDWAREBUFFER_FORMAT_R8G8B8_UNORM);
     printf("OSMDroid: width=%i;height=%i, reserving %i bytes for frame buffer\n", width, height,
            width * 4 * height);
     gbuffer = malloc(width * 4 * height+1);
@@ -272,14 +113,8 @@ ANativeWindow_Buffer buf;
 int32_t stride;
 void flipFrame() {
     ANativeWindow_lock(potatoBridge.androidWindow,&buf,NULL);
-
-    if(stride != buf.stride) {
-        stride = buf.stride;
-        printf("OSMDroid: stride reset\n");
-
-    }
     glFinish_p();
-    memcpy(buf.bits,gbuffer,width*4*height);
+    memcpy(buf.bits,gbuffer,stride*3*height);
     ANativeWindow_unlockAndPost(potatoBridge.androidWindow);
 }
 bool stopSwapBuffers;
@@ -297,7 +132,10 @@ JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglMakeCurrent(JNIEnv*
     //}
     printf("OSMDroid: making current\n");
     OSMesaMakeCurrent_p((OSMesaContext)window,gbuffer,GL_UNSIGNED_BYTE,width,height);
-    OSMesaPixelStore_p(OSMESA_ROW_LENGTH,atoi(getenv("OSMESA_IMAGE_OFFSET")));
+    ANativeWindow_lock(potatoBridge.androidWindow,&buf,NULL);
+    OSMesaPixelStore_p(OSMESA_ROW_LENGTH,buf.stride);
+    stride = buf.stride;
+    ANativeWindow_unlockAndPost(potatoBridge.androidWindow);
     OSMesaPixelStore_p(OSMESA_Y_UP,0);
     printf("OSMDroid: vendor: %s\n",glGetString_p(GL_VENDOR));
     printf("OSMDroid: renderer: %s\n",glGetString_p(GL_RENDERER));
@@ -316,7 +154,7 @@ Java_org_lwjgl_glfw_GLFW_nativeEglDetachOnCurrentThread(JNIEnv *env, jclass claz
 JNIEXPORT jlong JNICALL
 Java_org_lwjgl_glfw_GLFW_nativeEglCreateContext(JNIEnv *env, jclass clazz, jlong contextSrc) {
     printf("OSMDroid: generating context\n");
-    void* ctx = OSMesaCreateContext_p(OSMESA_RGBA,contextSrc);
+    void* ctx = OSMesaCreateContext_p(OSMESA_RGB,contextSrc);
     printf("OSMDroid: context=%p",ctx);
     return ctx;
 }
