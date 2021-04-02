eae6ee09b95392c0ccd04669206fa9edd02e9025
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-04-02 17:47:32 +0700

    Maybe correct the switch between gl4es and zink

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
@@ -86,12 +86,13 @@ public class LauncherPreferences
         for (String arg : PREF_CUSTOM_JAVA_ARGS.split(" ")) {
             if (arg.startsWith(argLwjglLibname)) {
                 // TODO together with EGL, currently force to OSMesa only if users leave set gl4es
-                if (arg.substring(argLwjglLibname.length()).startsWith("libgl4es_11")) {
+                if (PREF_RENDERER.equals("vulkan_zink") /* && arg.substring(argLwjglLibname.length()).startsWith("libgl4es_11") */) {
                     PREF_CUSTOM_JAVA_ARGS.replace(arg, argLwjglLibname + "libOSMesa_8.so");
+                    PREF_CUSTOM_OPENGL_LIBNAME = "libOSMesa_8.so";
+                } else {
+                    PREF_CUSTOM_OPENGL_LIBNAME = arg.substring(argLwjglLibname.length());
                 }
-
-                PREF_CUSTOM_OPENGL_LIBNAME = arg.substring(argLwjglLibname.length());
             }
         }
-	}
+    }
 }
