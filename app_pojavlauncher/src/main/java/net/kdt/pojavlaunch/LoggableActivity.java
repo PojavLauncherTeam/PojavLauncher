0085264717098744cda03b3720bcd9dbed33e260
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-06-22 10:09:46 +0700

    int index

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/LoggableActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/LoggableActivity.java
@@ -4,13 +4,14 @@ import java.util.Map;
 
 public abstract class LoggableActivity extends BaseActivity {
     public Map<String, String> jreReleaseList;
-    public boolean filteredSessionID = false;
+    private boolean filteredSessionID = false;
     public void appendToLog(String text) {
         appendToLog(text, true);
     }
     
     public void appendlnToLog(String text) {
         // Filter out Session ID here
+        int index;
         if (!filteredSessionID && (index = text.indexOf("(Session ID is ")) != -1) {
             text = text.substring(0, index) + "(Session ID is <censored>)";
             filteredSessionID = true;
