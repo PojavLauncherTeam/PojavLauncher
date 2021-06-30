f9050f52dee4d8ae351c274440b693815b276bc4
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-06-30 14:07:20 +0300

    Update MinecraftDownloaderTask.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloaderTask.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloaderTask.java
@@ -14,6 +14,7 @@ import net.kdt.pojavlaunch.prefs.*;
 import net.kdt.pojavlaunch.utils.*;
 import net.kdt.pojavlaunch.value.*;
 import org.apache.commons.io.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MinecraftDownloaderTask extends AsyncTask<String, String, Throwable>
  {
