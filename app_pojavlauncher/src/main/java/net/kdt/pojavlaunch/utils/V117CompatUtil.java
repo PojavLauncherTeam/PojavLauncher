4f892524dad8c8285bc818b8fcc3ce0e774a138e
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-08-04 13:39:25 +0700

    Fixed a NPE: fixes #1738, #1774

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/V117CompatUtil.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/V117CompatUtil.java
@@ -79,6 +79,12 @@ public class V117CompatUtil {
     }
      */
     private static List<String> getTexturePackList(String param) {
+        if (param == null) {
+            Log.i("V117CompatDebug","null, defaulting to vanilla");
+            List<String> arr = new ArrayList<>();
+            arr.add("vanilla");
+            return arr;
+        }
         Log.i("V117CompatDebug",param);
         if("[]".equals(param)) return new ArrayList<>();
         Log.i("V117CompatDebug","ph2");
