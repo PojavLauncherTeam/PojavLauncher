978f0299587735c2709d3e62c310f179faf1e492
SerpentSpirale <mathiasboulay@free.fr>
2021-09-07 19:33:46 +0200

    Clear focus when mouse grab is false

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/BaseMainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/BaseMainActivity.java
@@ -479,6 +479,7 @@ public class BaseMainActivity extends LoggableActivity {
                             CallbackBridge.mouseY = (int) mouse_y;
                             if(!CallbackBridge.isGrabbing()){
                                 view.releasePointerCapture();
+                                view.clearFocus();
                             }
 
                             if (debugText.getVisibility() == View.VISIBLE && !debugErrored) {
