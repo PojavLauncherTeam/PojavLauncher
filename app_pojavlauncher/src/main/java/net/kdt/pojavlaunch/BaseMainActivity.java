eec850dfcc1d339a441c7c5585efd2dcedd07283
SerpentSpirale <mathiasboulay@free.fr>
2021-09-09 18:32:48 +0200

    Fix buggy volume rocker key test

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/BaseMainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/BaseMainActivity.java
@@ -649,8 +649,8 @@ public class BaseMainActivity extends LoggableActivity {
         //Filtering useless events by order of probability
         if((event.getFlags() & KeyEvent.FLAG_FALLBACK) == KeyEvent.FLAG_FALLBACK) return true;
         if(event.getKeyCode() == KeyEvent.KEYCODE_UNKNOWN) return true;
-        if(event.getAction() == KeyEvent.KEYCODE_VOLUME_DOWN) return false;
-        if(event.getAction() == KeyEvent.KEYCODE_VOLUME_UP) return false;
+        if(event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) return false;
+        if(event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) return false;
         if(event.getRepeatCount() != 0) return true;
         if(event.getAction() == KeyEvent.ACTION_MULTIPLE) return true;
 
