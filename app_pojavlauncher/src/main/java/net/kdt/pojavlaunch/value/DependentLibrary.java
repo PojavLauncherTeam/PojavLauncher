19a1439fac1c4bc4b11b0fd66b38d9499948cec9
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-10-17 20:34:09 +0700

    Update DependentLibrary.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/DependentLibrary.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/DependentLibrary.java
@@ -1,16 +1,18 @@
 package net.kdt.pojavlaunch.value;
 
+import net.kdt.pojavlaunch.JMinecraftVersionList.Arguments.ArgValue.ArgRules;
+
 public class DependentLibrary {
+    public ArgRules[] rules;
     public String name;
-	public LibraryDownloads downloads;
+    public LibraryDownloads downloads;
     public String url;
     
-	public static class LibraryDownloads
-	{
-		public MinecraftLibraryArtifact artifact;
-		public LibraryDownloads(MinecraftLibraryArtifact artifact) {
-			this.artifact = artifact;
-		}
-	}
+    public static class LibraryDownloads {
+        public MinecraftLibraryArtifact artifact;
+            public LibraryDownloads(MinecraftLibraryArtifact artifact) {
+                this.artifact = artifact;
+            }
+    }
 }
 
