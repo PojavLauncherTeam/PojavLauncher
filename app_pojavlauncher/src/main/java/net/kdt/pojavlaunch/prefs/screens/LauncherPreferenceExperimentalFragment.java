ae8a067a20d8463635b254c805a2f47507bf4041
SerpentSpirale <mathiasboulay@free.fr>
2021-11-02 21:46:33 +0100

    Implement ExperimentalFragment in settings

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExperimentalFragment.java
@@ -0,0 +1,13 @@
+package net.kdt.pojavlaunch.prefs.screens;
+
+import android.os.Bundle;
+
+import net.kdt.pojavlaunch.R;
+
+public class LauncherPreferenceExperimentalFragment extends LauncherPreferenceFragment {
+
+    @Override
+    public void onCreatePreferences(Bundle b, String str) {
+        addPreferencesFromResource(R.xml.pref_experimental);
+    }
+}
