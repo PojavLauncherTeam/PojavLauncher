63e5b76df5fd0b25c287777ee94f76b2c8457d5e
Kucing Elit <nekoicecream@outlook.co.id>
2021-12-05 07:50:05 +0700

    Update languagelist_updater.bat

--- scripts/languagelist_updater.bat
+++ scripts/languagelist_updater.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set thisdir = "%~dp0"
-set langfile = %thisdir%\..\app_pojavlauncher\src\main\assets\language_list.txt
+set thisdir="%~dp0"
+set langfile=%thisdir%\..\app_pojavlauncher\src\main\assets\language_list.txt
 
 del %langfile%
 dir %thisdir%\..\app_pojavlauncher\src\main\res\values-* /s /b > %langfile%
