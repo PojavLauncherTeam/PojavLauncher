9fc4d5dc82eb37b260e6f6922ec974e236f1f18e
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-12-13 22:49:22 +0300

    Upstreamify profiles (#2390)
    * Extended MCOptionUtils to support list values
    
    * Mitigation for the Apache Log4j RCE in 1.8.x-1.18
    
    Related issue: #2378
    
    * Update JREUtils.java
    
    Co-authored-by: SerpentSpirale <mathiasboulay@free.fr>
    Co-authored-by: Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -361,6 +361,8 @@ public class JREUtils {
 
                 "-Dext.net.resolvPath=" +new File(Tools.DIR_DATA,"resolv.conf").getAbsolutePath(),
 
+                "-Dlog4j2.formatMsgNoLookups=true", //Log4j RCE mitigation
+
                 "-Dnet.minecraft.clientmodname=" + Tools.APP_NAME,
                 "-Dfml.earlyprogresswindow=false" //Forge 1.14+ workaround
         };
@@ -538,7 +540,6 @@ public class JREUtils {
     public static native void setupExitTrap(Context context);
     // Obtain AWT screen pixels to render on Android SurfaceView
     public static native int[] renderAWTScreenFrame(/* Object canvas, int width, int height */);
-
     static {
         System.loadLibrary("pojavexec");
         System.loadLibrary("pojavexec_awt");
