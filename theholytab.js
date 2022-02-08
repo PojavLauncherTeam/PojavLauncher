8034904041b1eb51582f005d5771fe21c26b95b7
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2022-02-08 20:52:04 +0700

    Update stuff (#2653) + attempt to recover lost translations

---
+++ theholytab.js
@@ -0,0 +1,13 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
