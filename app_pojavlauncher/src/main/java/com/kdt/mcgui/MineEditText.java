8ba52529bdac29b6520fbfab6d7b74bbb41e5076
SerpentSpirale <mathiasboulay@free.fr>
2022-02-26 17:35:09 +0100

    Update MineEditText.java

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
@@ -4,22 +4,18 @@ import android.content.*;
 import android.util.*;
 import android.graphics.*;
 
-public class MineEditText extends com.google.android.material.textfield.TextInputEditText
-{
-	public MineEditText(Context ctx)
-	{
+public class MineEditText extends com.google.android.material.textfield.TextInputEditText {
+	public MineEditText(Context ctx) {
 		super(ctx);
 		init();
 	}
 
-	public MineEditText(Context ctx, AttributeSet attrs)
-	{
+	public MineEditText(Context ctx, AttributeSet attrs) {
 		super(ctx, attrs);
 		init();
 	}
 
-	public void init()
-	{
+	public void init() {
 		setBackgroundColor(Color.parseColor("#131313"));
 		setPadding(5, 5, 5, 5);
 	}
