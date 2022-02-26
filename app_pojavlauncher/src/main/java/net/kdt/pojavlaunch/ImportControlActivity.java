cc26e7fa23a4a0a5bd16749e759ce76a47594dd9
SerpentSpirale <mathiasboulay@free.fr>
2022-02-26 16:05:52 +0100

    Update ImportControlActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ImportControlActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ImportControlActivity.java
@@ -185,12 +185,10 @@ public class ImportControlActivity extends Activity {
             String jsonLayoutData = Tools.read(Tools.CTRLMAP_PATH + "/TMP_IMPORT_FILE.json");
             JSONObject layoutJobj = new JSONObject(jsonLayoutData);
             return layoutJobj.has("version") && layoutJobj.has("mControlDataList");
-
         }catch (JSONException | IOException e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
     public String getNameFromURI(Uri uri) {
