0be552c9ff5ac8b5f0c0d2f5c45baeac588529cd
Maniac Gamer156 <colepatrickroberts@gmail.com>
2022-03-13 23:09:14 -0700

    Change renderImage C++ function to match MCXR, make the video renderer we use default

--- app_pojavlauncher/src/main/jni/mcxr_loader.cpp
+++ app_pojavlauncher/src/main/jni/mcxr_loader.cpp
@@ -25,8 +25,8 @@ Java_net_sorenon_mcxr_play_MCXRNativeLoad_getJVMPtr(JNIEnv *env, jclass clazz) {
 
 JNIEXPORT JNICALL
 extern "C" void
-Java_net_sorenon_mcxr_play_MCXRNativeLoad_renderImage(JNIEnv *env, jclass clazz, jint colorAttachment) {
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment, 0);
+Java_net_sorenon_mcxr_play_MCXRNativeLoad_renderImage(JNIEnv *env, jclass clazz, jint colorAttachment, jint index) {
+    glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, colorAttachment, 0, index)
 }
 
 JNIEXPORT JNICALL
