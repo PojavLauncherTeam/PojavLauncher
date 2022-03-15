1cb369858b38000270b0ef6c1dd1397d37b20b6d
Gus Bunce <70128027+gusbunce@users.noreply.github.com>
2022-03-15 12:45:23 +1000

    Update PojavLoginActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
@@ -335,6 +335,7 @@ public class PojavLoginActivity extends BaseActivity {
             Tools.copyAssetFile(this, "artifacts/mcxr-play-0.2.0+null.jar", DIR_GAME_NEW + "/mods", false);
             Tools.copyAssetFile(this, "artifacts/titleworlds-0.0.2.jar", DIR_GAME_NEW + "/mods", false);
             Tools.copyAssetFile(this, "artifacts/lazydfu-0.1.2.jar", DIR_GAME_NEW + "/mods", false);
+            Tools.copyAssetFile(this, "artifacts/fabric-api-0.48.0+1.18.2.jar", DIR_GAME_NEW + "/mods", false);
 
             AssetManager am = this.getAssets();
 
