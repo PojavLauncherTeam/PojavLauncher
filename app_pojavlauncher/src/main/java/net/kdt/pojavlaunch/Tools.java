71e71691ae210ed5839e3dcb68a3e6cafcfbd6b4
DJ <84008186+CADIndie@users.noreply.github.com>
2022-03-17 13:59:43 -0400

    Change News | Update Name

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -43,7 +43,7 @@ public final class Tools {
     
     public static final Gson GLOBAL_GSON = new GsonBuilder().setPrettyPrinting().create();
     
-    public static final String URL_HOME = "https://pojavlauncherteam.github.io/PojavLauncher";
+    public static final String URL_HOME = "https://quest-craft.com/changelog/";
 
     public static String DIR_DATA; //Initialized later to get context
     public static String MULTIRT_HOME;
