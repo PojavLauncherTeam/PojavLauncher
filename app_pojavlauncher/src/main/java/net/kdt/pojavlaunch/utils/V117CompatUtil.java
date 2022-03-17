4e6677b2494d5725bf91fa6f45aa64e210c116ec
DJ <84008186+CADIndie@users.noreply.github.com>
2022-03-17 17:48:08 -0400

    Fix my mistake...

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/V117CompatUtil.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/V117CompatUtil.java
@@ -31,6 +31,7 @@ public class V117CompatUtil {
                         New rendering engine was added in snapshot 21w10a
                         21a08b (FP (GL2) engine): 20210225
                         21w10a (non-FP engine): 20210310
+
     boolean skipResDialog = false;
                     if(mcReleaseDate > 20210225) skipResDialog = true;
                     PerVersionConfig.update(); //Prepare the PVC
@@ -40,6 +41,7 @@ public class V117CompatUtil {
         PerVersionConfig.configMap.put(p1[0], cfg);//and put it into the base
     }
                     MCOptionUtils.load();
+
                     if(!skipResDialog) {
         AtomicBoolean proceed = new AtomicBoolean(false);
         Object lock = new Object();
@@ -78,51 +80,68 @@ public class V117CompatUtil {
      */
     private static List<String> getTexturePackList(String param) {
         if (param == null) {
-            Log.i("V117CompatDebug", "null, defaulting to empty");
+            Log.i("V117CompatDebug","null, defaulting to empty");
             return new ArrayList<>();
         }
-        Log.i("V117CompatDebug", param);
-        if ("[]".equals(param)) return new ArrayList<>();
-        Log.i("V117CompatDebug", "ph2");
-        if (param == null) return new ArrayList<>();
-        Log.i("V117CompatDebug", "ph3");
-        String rawList = param.substring(1, param.length() - 1);
-        Log.i("V117CompatDebug", rawList);
+        Log.i("V117CompatDebug",param);
+        if("[]".equals(param)) return new ArrayList<>();
+        Log.i("V117CompatDebug","ph2");
+        if(param == null) return new ArrayList<>();
+        Log.i("V117CompatDebug","ph3");
+        String rawList = param.substring(1,param.length()-1);
+        Log.i("V117CompatDebug",rawList);
         return new ArrayList<>(Arrays.asList(rawList.split(",")));
     }
 
     private static String regenPackList(List<String> packs) {
-        if (packs.size() == 0) return "[]";
+        if(packs.size()==0) return "[]";
         StringBuilder ret = new StringBuilder("[" + packs.get(0));
-        for (int i = 1; i < packs.size(); i++) {
+        for(int i = 1; i < packs.size(); i++) {
             ret.append(",").append(packs.get(i));
         }
         ret.append("]");
         return ret.toString();
     }
 
-    public static void runCheck(String version, Activity ctx) throws Exception {
+    public static void runCheck(String version, Activity ctx) throws Exception{
 
         PerVersionConfig.VersionConfig cfg = PerVersionConfig.configMap.get(version);
         MCOptionUtils.load();
 
-        List<String> packList = getTexturePackList(MCOptionUtils.get("resourcePacks"));
-        String renderer = cfg != null && cfg.renderer != null ? cfg.renderer : LauncherPreferences.PREF_RENDERER;
+        List<String> packList =getTexturePackList(MCOptionUtils.get("resourcePacks"));
+        String renderer = cfg != null && cfg.renderer != null?cfg.renderer:LauncherPreferences.PREF_RENDERER;
 
-        if (renderer.equals("vulkan_zink") || renderer.equals("opengles3_virgl"))
-            return; //don't install for zink/virgl users;
-        if (packList.contains("\"assets-v0.zip\"") && renderer.equals("opengles3")) return;
+        if(renderer.equals("vulkan_zink") || renderer.equals("opengles3_virgl")) return; //don't install for zink/virgl users;
+        if(packList.contains("\"assets-v0.zip\"") && renderer.equals("opengles3")) return;
 
         Object lock = new Object();
         AtomicInteger proceed = new AtomicInteger(0);
         ctx.runOnUiThread(() -> {
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+            builder.setTitle(R.string.global_warinng);
+            builder.setMessage(R.string.compat_117_message);
+            builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                proceed.set(1);
+                synchronized (lock) { lock.notifyAll(); }
+                dialog.dismiss();
+            });
+            builder.setNegativeButton(android.R.string.cancel, (dialog, which) -> {
+                synchronized (lock) { lock.notifyAll(); }
+                dialog.dismiss();
+            });
+            builder.setNeutralButton(R.string.compat_11x_playanyway, (dialog, which) -> {
+                proceed.set(2);
+                synchronized (lock) { lock.notifyAll(); }
+                dialog.dismiss();
+            });
+            builder.setCancelable(false);
+            builder.show();
         });
 
         synchronized (lock) {
             lock.wait();
         }
-        switch (proceed.get()) {
+        switch(proceed.get()) {
             case 1:
                 if (cfg == null) {
                     cfg = new PerVersionConfig.VersionConfig();
@@ -130,10 +149,10 @@ public class V117CompatUtil {
                 }
                 cfg.renderer = "opengles3";
                 String path = Tools.DIR_GAME_NEW;
-                if (cfg.gamePath != null && !cfg.gamePath.isEmpty()) path = cfg.gamePath;
-                copyResourcePack(path, ctx.getAssets());
-                if (!packList.contains("\"assets-v0.zip\"")) packList.add(0, "\"assets-v0.zip\"");
-                MCOptionUtils.set("resourcePacks", regenPackList(packList));
+                if(cfg.gamePath != null && !cfg.gamePath.isEmpty()) path = cfg.gamePath;
+                copyResourcePack(path,ctx.getAssets());
+                if(!packList.contains("\"assets-v0.zip\"")) packList.add(0,"\"assets-v0.zip\"");
+                MCOptionUtils.set("resourcePacks",regenPackList(packList));
                 MCOptionUtils.save();
                 PerVersionConfig.update();
                 break;
@@ -143,12 +162,11 @@ public class V117CompatUtil {
     }
 
     public static void copyResourcePack(String gameDir, AssetManager am) throws IOException {
-        File resourcepacksDir = new File(gameDir, "resourcepacks");
-        if (!resourcepacksDir.exists()) resourcepacksDir.mkdirs();
-        FileOutputStream fos = new FileOutputStream(new File(resourcepacksDir, "assets-v0.zip"));
+        File resourcepacksDir = new File(gameDir,"resourcepacks");
+        if(!resourcepacksDir.exists()) resourcepacksDir.mkdirs();
+        FileOutputStream fos = new FileOutputStream(new File(resourcepacksDir,"assets-v0.zip"));
         InputStream is = am.open("assets-v0.zip");
-        IOUtils.copy(is, fos);
-        is.close();
-        fos.close();
+        IOUtils.copy(is,fos);
+        is.close();fos.close();
     }
 }
