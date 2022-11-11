1372b08bed64d257a75963b851dbb351266ff7ed
artdeell <ultramaksim@gmail.com>
2022-11-11 09:57:36 +0300

    VUI fixes & improvements pt.3.5
    Fixed the controls layout being offset on first start
    Fixed the controls layout crashing when loading new control layout

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -500,7 +500,7 @@ public final class Tools {
 
     public static void setFullscreen(Activity activity, boolean fullscreen) {
         final View decorView = activity.getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener (visibility -> {
+        View.OnSystemUiVisibilityChangeListener visibilityChangeListener = visibility -> {
             if(fullscreen){
                 if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
                     decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -514,7 +514,9 @@ public final class Tools {
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
             }
 
-        });
+        };
+        decorView.setOnSystemUiVisibilityChangeListener(visibilityChangeListener);
+        visibilityChangeListener.onSystemUiVisibilityChange(decorView.getSystemUiVisibility()); //call it once since the UI state may not change after the call, so the activity wont become fullscreen
     }
 
     public static DisplayMetrics currentDisplayMetrics;
