fb20f702460a1c6c2ebbbe880148f3bd22b34ebf
artdeell <ultramaksim@gmail.com>
2022-11-14 17:26:29 +0300

    Notification icon

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/GameService.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/services/GameService.java
@@ -49,7 +49,7 @@ public class GameService extends Service {
                 .setContentTitle(getString(R.string.lazy_service_default_title))
                 .setContentText(getString(R.string.notification_game_runs))
                 .addAction(android.R.drawable.ic_menu_close_clear_cancel,  getString(R.string.notification_terminate), pendingKillIntent)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setSmallIcon(R.drawable.notif_icon)
                 .setNotificationSilent();
        startForeground(2, notificationBuilder.build());
         return START_NOT_STICKY; // non-sticky so android wont try restarting the game after the user uses the "Quit" button
