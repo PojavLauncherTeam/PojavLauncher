57521c19609cd0ce7bd24e75f07e0cf10e01a3fd
Mathias-Boulay <mathiasboulay@free.fr>
2023-04-11 23:11:02 +0200

    Feat[profiles]: Avoid named profiles to be erased by installers

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/LauncherProfiles.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/value/launcherprofiles/LauncherProfiles.java
@@ -1,8 +1,12 @@
 package net.kdt.pojavlaunch.value.launcherprofiles;
+import android.util.Log;
+
 import com.google.gson.*;
 import net.kdt.pojavlaunch.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class LauncherProfiles {
     public static MinecraftLauncherProfiles mainProfileJson;
@@ -13,6 +17,9 @@ public class LauncherProfiles {
                 if (launcherProfilesFile.exists()) {
                     mainProfileJson = Tools.GLOBAL_GSON.fromJson(Tools.read(launcherProfilesFile.getAbsolutePath()), MinecraftLauncherProfiles.class);
                     if(mainProfileJson.profiles == null) mainProfileJson.profiles = new HashMap<>();
+                    else if(LauncherProfiles.normalizeProfileIds(mainProfileJson)){
+                        LauncherProfiles.update();
+                    }
                 } else {
                     mainProfileJson = new MinecraftLauncherProfiles();
                     mainProfileJson.profiles = new HashMap<>();
@@ -26,6 +33,40 @@ public class LauncherProfiles {
         } catch (Throwable th) {
             throw new RuntimeException(th);
         }
+    }
+
+    /**
+     * For all keys to be UUIDs, effectively isolating profile created by installers
+     * This avoids certain profiles to be erased by the installer
+     * @return Whether some profiles have been normalized
+     */
+    private static boolean normalizeProfileIds(MinecraftLauncherProfiles launcherProfiles){
+        boolean hasNormalized = false;
+        ArrayList<String> keys = new ArrayList<>();
+
+        // Detect denormalized keys
+        for(String profileKey : launcherProfiles.profiles.keySet()){
+            try{
+                UUID.fromString(profileKey);
+            }catch (IllegalArgumentException exception){
+                keys.add(profileKey);
+                Log.w(LauncherProfiles.class.toString(), "Illegal profile uuid: " + profileKey);
+            }
+        }
+
+        // Swap the new keys
+        for(String profileKey: keys){
+            String uuid = UUID.randomUUID().toString();
+            while(launcherProfiles.profiles.containsKey(uuid)) {
+                uuid = UUID.randomUUID().toString();
+            }
+
+            launcherProfiles.profiles.put(uuid, launcherProfiles.profiles.get(profileKey));
+            launcherProfiles.profiles.remove(profileKey);
+            hasNormalized = true;
+        }
+
+        return hasNormalized;
     }
     /*
      public static String insert;
