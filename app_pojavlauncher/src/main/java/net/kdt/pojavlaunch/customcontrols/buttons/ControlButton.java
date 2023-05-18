6e194302f5e8a5c253ef4d10df2cd61349c76c47
Mathias-Boulay <mathiasboulay@free.fr>
2023-05-19 00:39:08 +0200

    Fix[controls]: incorrect activated overlay on rounded controls

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/buttons/ControlButton.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/buttons/ControlButton.java
@@ -31,6 +31,9 @@ public class ControlButton extends TextView implements ControlInterface {
     protected ControlData mProperties;
     private final ControlLayout mControlLayout;
 
+    /* Cache value from the ControlData radius for drawing purposes */
+    private float mComputedRadius;
+
     protected boolean mIsToggled = false;
     protected boolean mIsPointerOutOfBounds = false;
 
@@ -61,6 +64,7 @@ public class ControlButton extends TextView implements ControlInterface {
     public void setProperties(ControlData properties, boolean changePos) {
         mProperties = properties;
         ControlInterface.super.setProperties(properties, changePos);
+        mComputedRadius = ControlInterface.super.computeCornerRadius(mProperties.cornerRadius);
 
         if (mProperties.isToggle) {
             //For the toggle layer
@@ -85,7 +89,7 @@ public class ControlButton extends TextView implements ControlInterface {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (mIsToggled || (!mProperties.isToggle && isActivated()))
-            canvas.drawRoundRect(0, 0, getWidth(), getHeight(), mProperties.cornerRadius, mProperties.cornerRadius, mRectPaint);
+            canvas.drawRoundRect(0, 0, getWidth(), getHeight(), mComputedRadius, mComputedRadius, mRectPaint);
     }
 
 
