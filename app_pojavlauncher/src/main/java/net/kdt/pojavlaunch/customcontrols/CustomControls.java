9f7a580019c8b3d055177042878809b2e8254612
DreamOneX <me@dreamonex.eu.org>
2023-08-02 21:13:49 +0800

    chore(control): bumb up the version number of control schema

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/CustomControls.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/CustomControls.java
@@ -52,13 +52,13 @@ public class CustomControls {
 		this.mControlDataList.add(new ControlData(ctx, R.string.control_jump, new int[]{LwjglGlfwKeycode.GLFW_KEY_SPACE}, "${right} - ${margin} * 2 - ${width}", "${bottom} - ${margin} * 2 - ${height}", true));
 
 		//The default controls are conform to the V2
-		version = 4;
+		version = 5;
 	}
 
 	
 	public void save(String path) throws IOException {
-		//Current version is the V2.5 so the version as to be marked as 4 !
-		version = 4;
+		//Current version is the V2.6 so the version as to be marked as 5 !
+		version = 5;
 
 		Tools.write(path, Tools.GLOBAL_GSON.toJson(this));
 	}
