8a70c6f4f521adeb6e32b1994169f558b7f24bcb
DreamOneX <me@dreamonex.eu.org>
2023-08-03 18:10:29 +0800

    fix(control): not allowed to open the control file of version 5

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/LayoutConverter.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/LayoutConverter.java
@@ -28,7 +28,7 @@ public class LayoutConverter {
                 CustomControls layout = LayoutConverter.convertV2Layout(layoutJobj);
                 layout.save(jsonPath);
                 return layout;
-            }else if (layoutJobj.getInt("version") == 3 || layoutJobj.getInt("version") == 4) {
+            }else if (layoutJobj.getInt("version") >= 3 && layoutJobj.getInt("version") <= 5) {
                 return Tools.GLOBAL_GSON.fromJson(jsonLayoutData, CustomControls.class);
             }else{
                 return null;
