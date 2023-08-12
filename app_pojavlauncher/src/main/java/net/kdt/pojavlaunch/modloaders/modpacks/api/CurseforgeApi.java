27a1aabaad69ce427d415c5f5986f6bd0cdb1375
BuildTools <unconfigured@null.spigotmc.org>
2023-08-12 23:11:51 +0300

    CurseForge modpack download (WIP) + more generalization

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/CurseforgeApi.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/CurseforgeApi.java
@@ -5,14 +5,16 @@ import android.util.Log;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.kdt.mcgui.ProgressLayout;
 
+import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.Constants;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.CurseManifest;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.ModDetail;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.ModItem;
-import net.kdt.pojavlaunch.modloaders.modpacks.models.ModrinthIndex;
 import net.kdt.pojavlaunch.modloaders.modpacks.models.SearchFilters;
+import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 import net.kdt.pojavlaunch.utils.ZipUtils;
 
 import java.io.File;
@@ -31,6 +33,8 @@ public class CurseforgeApi implements ModpackApi{
     // https://api.curseforge.com/v1/categories?gameId=432 and search for "Mods" (case-sensitive)
     private static final int CURSEFORGE_MOD_CLASS_ID = 6;
     private static final int CURSEFORGE_PAGINATION_SIZE = 50;
+    private static final int CURSEFORGE_PAGINATION_END_REACHED = -1;
+    private static final int CURSEFORGE_PAGINATION_ERROR = -2;
 
     private final ApiHandler mApiHandler = new ApiHandler("https://api.curseforge.com/v1", "$2a$10$Vxkj4kH1Ekf8EsS4Mx8b2eVTHsht107Lk2erVEUtnbqvojsLy.jYq");
 
@@ -71,10 +75,11 @@ public class CurseforgeApi implements ModpackApi{
     public ModDetail getModDetails(ModItem item) {
         ArrayList<JsonObject> allModDetails = new ArrayList<>();
         int index = 0;
-        while(index != -1 && index != -2) {
+        while(index != CURSEFORGE_PAGINATION_END_REACHED &&
+                index != CURSEFORGE_PAGINATION_ERROR) {
             index = getPaginatedDetails(allModDetails, index, item.id);
         }
-        if(index == -2) return null;
+        if(index == CURSEFORGE_PAGINATION_ERROR) return null;
         int length = allModDetails.size();
         String[] versionNames = new String[length];
         String[] mcVersionNames = new String[length];
@@ -99,8 +104,8 @@ public class CurseforgeApi implements ModpackApi{
 
     @Override
     public void installMod(ModDetail modDetail, int selectedVersion) {
-        String versionUrl = modDetail.versionUrls[selectedVersion];
-        File modpackFile = new File(Tools.DIR_CACHE, modDetail.id+".zip");
+        //TODO considering only modpacks for now
+        ModpackInstaller.installModpack(modDetail, selectedVersion, this::installCurseforgeZip);
     }
 
 
@@ -109,16 +114,16 @@ public class CurseforgeApi implements ModpackApi{
         params.put("index", index);
         params.put("pageSize", CURSEFORGE_PAGINATION_SIZE);
         JsonObject response = mApiHandler.get("mods/"+modId+"/files", params, JsonObject.class);
-        if(response == null) return -2;
+        if(response == null) return CURSEFORGE_PAGINATION_ERROR;
         JsonArray data = response.getAsJsonArray("data");
-        if(data == null) return -2;
+        if(data == null) return CURSEFORGE_PAGINATION_ERROR;
         for(int i = 0; i < data.size(); i++) {
             JsonObject fileInfo = data.get(i).getAsJsonObject();
             if(fileInfo.get("isServerPack").getAsBoolean()) continue;
             objectList.add(fileInfo);
         }
         if(data.size() < CURSEFORGE_PAGINATION_SIZE) {
-            return -1; // we read the remainder! yay!
+            return CURSEFORGE_PAGINATION_END_REACHED; // we read the remainder! yay!
         }
         return index + data.size();
     }
@@ -128,7 +133,72 @@ public class CurseforgeApi implements ModpackApi{
             CurseManifest curseManifest = Tools.GLOBAL_GSON.fromJson(
                     Tools.read(ZipUtils.getEntryStream(modpackZipFile, "manifest.json")),
                     CurseManifest.class);
+            if(!verifyManifest(curseManifest)) {
+                Log.i("CurseforgeApi","manifest verification failed");
+                return null;
+            }
+            ModDownloader modDownloader = new ModDownloader(new File(instanceDestination,"mods"), true);
+            int fileCount = curseManifest.files.length;
+            for(int i = 0; i < fileCount; i++) {
+                CurseManifest.CurseFile curseFile = curseManifest.files[i];
+                String downloadUrl = getDownloadUrl(curseFile.projectID, curseFile.fileID);
+                if(downloadUrl == null && curseFile.required) throw new IOException("Failed to obtain download URL for "+curseFile.projectID+" "+curseFile.fileID);
+                else if(downloadUrl == null) continue;
+                modDownloader.submitDownload(Tools.getFileName(downloadUrl), downloadUrl);
+                ProgressKeeper.submitProgress(ProgressLayout.INSTALL_MODPACK, (int) Math.max((float)i/fileCount*100,0), R.string.modpack_download_getting_links, i, fileCount);
+            }
+            modDownloader.awaitFinish((c,m)->{ // insert joke about semen
+                ProgressKeeper.submitProgress(ProgressLayout.INSTALL_MODPACK, (int) Math.max((float)c/m*100,0), R.string.modpack_download_downloading_mods_fc, c, m);
+            });
+            String overridesDir = "overrides";
+            if(curseManifest.overrides != null) overridesDir = curseManifest.overrides;
+            ZipUtils.zipExtract(modpackZipFile, overridesDir, instanceDestination);
+            return createInfo(curseManifest.minecraft);
         }
-        return null;
+    }
+
+    private ModLoaderInfo createInfo(CurseManifest.CurseMinecraft minecraft) {
+        CurseManifest.CurseModLoader primaryModLoader = null;
+        for(CurseManifest.CurseModLoader modLoader : minecraft.modLoaders) {
+            if(modLoader.primary) {
+                primaryModLoader = modLoader;
+                break;
+            }
+        }
+        if(primaryModLoader == null) primaryModLoader = minecraft.modLoaders[0];
+        String modLoaderId = primaryModLoader.id;
+        int dashIndex = modLoaderId.indexOf('-');
+        String modLoaderName = modLoaderId.substring(0, dashIndex);
+        String modLoaderVersion = modLoaderId.substring(dashIndex+1);
+        int modLoaderTypeInt = -1;
+        switch (modLoaderName) {
+            case "forge":
+                modLoaderTypeInt = ModLoaderInfo.MOD_LOADER_FORGE;
+                break;
+            case "fabric":
+                modLoaderTypeInt = ModLoaderInfo.MOD_LOADER_FABRIC;
+                break;
+            //TODO: Quilt is also Forge? How does that work?
+        }
+        if(modLoaderTypeInt == -1) return null;
+        return new ModLoaderInfo(modLoaderTypeInt, modLoaderVersion, minecraft.version);
+    }
+
+    private String getDownloadUrl(long projectID, long fileID) {
+        JsonObject response = mApiHandler.get("mods/"+projectID+"/files/"+fileID+"/download-url", JsonObject.class);
+        if(response == null) return null;
+        JsonElement data = response.get("data");
+        if(data == null || data.isJsonNull()) return null;
+        return data.getAsString();
+    }
+
+    private boolean verifyManifest(CurseManifest manifest) {
+        if(!"minecraftModpack".equals(manifest.manifestType)) return false;
+        if(manifest.manifestVersion != 1) return false;
+        if(manifest.minecraft == null) return false;
+        if(manifest.minecraft.version == null) return false;
+        if(manifest.minecraft.modLoaders == null) return false;
+        if(manifest.minecraft.modLoaders.length < 1) return false;
+        return true;
     }
 }
