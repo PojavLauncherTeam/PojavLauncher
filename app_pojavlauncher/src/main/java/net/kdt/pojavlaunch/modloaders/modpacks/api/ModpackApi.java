a7da51abc5f6ed514679ae4038524536fc036321
BuildTools <unconfigured@null.spigotmc.org>
2023-08-14 17:55:08 +0300

    More resilient mod loader installation for the modpack installer

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/ModpackApi.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/ModpackApi.java
@@ -40,25 +40,7 @@ public interface ModpackApi {
         PojavApplication.sExecutorService.execute(() -> {
             ModLoader loaderInfo = installMod(modDetail, selectedVersion);
             if (loaderInfo == null) return;
-
-            loaderInfo.getDownloadTask(new ModloaderDownloadListener() {
-                @Override
-                public void onDownloadFinished(File downloadedFile) {
-                    Intent intent = loaderInfo.getInstallationIntent(context, downloadedFile);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(intent);
-                }
-
-                @Override
-                public void onDataNotAvailable() {
-                    Toast.makeText(context, "Version gathering failed", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onDownloadError(Exception e) {
-                    Toast.makeText(context, "download failed", Toast.LENGTH_SHORT).show();
-                }
-            }).run();
+            loaderInfo.getDownloadTask(new NotificationDownloadListener(context, loaderInfo)).run();
         });
     }
 
---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/api/NotificationDownloadListener.java
@@ -0,0 +1,66 @@
+package net.kdt.pojavlaunch.modloaders.modpacks.api;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import net.kdt.pojavlaunch.LauncherActivity;
+import net.kdt.pojavlaunch.R;
+import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.modloaders.ModloaderDownloadListener;
+import net.kdt.pojavlaunch.modloaders.modpacks.ModloaderInstallTracker;
+
+import java.io.File;
+
+public class NotificationDownloadListener implements ModloaderDownloadListener {
+
+    private final NotificationCompat.Builder mNotificationBuilder;
+    private final NotificationManager mNotificationManager;
+    private final Context mContext;
+    private final ModLoader mModLoader;
+    
+    public NotificationDownloadListener(Context context, ModLoader modLoader) {
+        mModLoader = modLoader;
+        mContext = context.getApplicationContext();
+        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationBuilder = new NotificationCompat.Builder(context, "channel_id")
+                .setContentTitle(context.getString(R.string.modpack_install_notification_title))
+                .setSmallIcon(R.drawable.notif_icon);
+    }
+
+    @Override
+    public void onDownloadFinished(File downloadedFile) {
+        ModloaderInstallTracker.saveModLoader(mContext, mModLoader, downloadedFile);
+        Intent mainActivityIntent = new Intent(mContext, LauncherActivity.class);
+        Tools.runOnUiThread(()->sendIntentNotification(mainActivityIntent, R.string.modpack_install_notification_success));
+    }
+
+    @Override
+    public void onDataNotAvailable() {
+        Tools.runOnUiThread(()->sendEmptyNotification(R.string.modpack_install_notification_data_not_available));
+    }
+
+    @Override
+    public void onDownloadError(Exception e) {
+        Tools.runOnUiThread(()->sendEmptyNotification(R.string.modpack_install_notification_download_failed));
+    }
+    
+    private void sendIntentNotification(Intent intent, int contentText) {
+        PendingIntent pendingInstallIntent =
+                PendingIntent.getActivity(mContext, 0,
+                        intent, Build.VERSION.SDK_INT >=23 ? PendingIntent.FLAG_IMMUTABLE : 0);
+
+        mNotificationBuilder.setContentText(mContext.getText(contentText));
+        mNotificationBuilder.setContentIntent(pendingInstallIntent);
+        mNotificationManager.notify(3, mNotificationBuilder.build());
+    }
+
+    private void sendEmptyNotification(int contentText) {
+        mNotificationBuilder.setContentText(mContext.getText(contentText));
+        mNotificationManager.notify(3, mNotificationBuilder.build());
+    }
+}
