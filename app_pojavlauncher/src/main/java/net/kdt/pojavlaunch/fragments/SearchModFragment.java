04d6900bc49b573e161646dcdd0d29e391df2bf0
Mathias Boulay <mathiasboulay@free.fr>
2023-08-17 00:15:37 +0200

    Feat, wip[UI]: conditionnal visibility, reduced layout shifting, full screen scroll

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SearchModFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SearchModFragment.java
@@ -2,6 +2,7 @@ package net.kdt.pojavlaunch.fragments;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.math.MathUtils;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,6 +37,16 @@ import java.util.concurrent.Future;
 public class SearchModFragment extends Fragment {
 
     public static final String TAG = "SearchModFragment";
+    private View mOverlay;
+    private float mOverlayTopCache; // Padding cache reduce resource lookup
+
+    private final RecyclerView.OnScrollListener mOverlayPositionListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            mOverlay.setY(MathUtils.clamp(mOverlay.getY() - dy, -mOverlay.getHeight(), mOverlayTopCache));
+        }
+    };
+
     private TextView mSelectedVersion;
     private Button mSelectVersionButton;
     private EditText mSearchEditText;
@@ -49,8 +61,6 @@ public class SearchModFragment extends Fragment {
 
     private SearchFilters mSearchFilters;
 
-
-
     public SearchModFragment(){
         super(R.layout.fragment_mod_search);
         modpackApi = new CommonApi();
@@ -62,7 +72,9 @@ public class SearchModFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         // You can only access resources after attaching to current context
         mModItemAdapter = new ModItemAdapter(getResources(), modpackApi);
+        mOverlayTopCache = getResources().getDimension(R.dimen.fragment_padding_medium);
 
+        mOverlay = view.findViewById(R.id.search_mod_overlay);
         mSearchEditText = view.findViewById(R.id.search_mod_edittext);
         mSearchProgressBar = view.findViewById(R.id.search_mod_progressbar);
         mSelectedVersion = view.findViewById(R.id.search_mod_selected_mc_version_textview);
@@ -74,6 +86,8 @@ public class SearchModFragment extends Fragment {
 
         mRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
         mRecyclerview.setAdapter(mModItemAdapter);
+
+        mRecyclerview.addOnScrollListener(mOverlayPositionListener);
 
         // Setup the expendable list behavior
         mSelectVersionButton.setOnClickListener(v -> VersionSelectorDialog.open(v.getContext(), true, (id, snapshot)->{
@@ -91,6 +105,12 @@ public class SearchModFragment extends Fragment {
                     .startOnExecutor(PojavApplication.sExecutorService);
             return true;
         });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mRecyclerview.removeOnScrollListener(mOverlayPositionListener);
     }
 
     class SearchModTask implements SelfReferencingFuture.FutureInterface {
