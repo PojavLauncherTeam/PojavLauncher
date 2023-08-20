8e237e68f478a25df65937178ce86068f2c368ff
Mathias Boulay <mathiasboulay@free.fr>
2023-08-20 15:36:23 +0200

    Cut: redundant zip handling methods

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/FileUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/FileUtils.java
@@ -12,52 +12,9 @@ public class FileUtils {
         return new File(filePath).exists();
     }
 
-    public static void uncompressZip(File filePath, File output) throws IOException {
-        final byte[] buffer = new byte[8192];
-        final ZipInputStream zis = new ZipInputStream(new FileInputStream(filePath));
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            final File newFile = newFile(output, zipEntry);
-            if (zipEntry.isDirectory()) {
-                if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                    throw new IOException("Failed to create directory " + newFile);
-                }
-            } else {
-                File parent = newFile.getParentFile();
-                if (!parent.isDirectory() && !parent.mkdirs()) {
-                    throw new IOException("Failed to create directory " + parent);
-                }
-
-                final FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
-                }
-                fos.close();
-            }
-            zipEntry = zis.getNextEntry();
-        }
-        zis.closeEntry();
-        zis.close();
-    }
-
     public static String getFileName(String pathOrUrl) {
         int lastSlashIndex = pathOrUrl.lastIndexOf('/');
         if(lastSlashIndex == -1) return null;
         return pathOrUrl.substring(lastSlashIndex);
     }
-
-    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
-        File destFile = new File(destinationDir, zipEntry.getName());
-
-        String destDirPath = destinationDir.getCanonicalPath();
-        String destFilePath = destFile.getCanonicalPath();
-
-        if (!destFilePath.startsWith(destDirPath + File.separator)) {
-            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
-        }
-
-        return destFile;
-    }
-
 }
