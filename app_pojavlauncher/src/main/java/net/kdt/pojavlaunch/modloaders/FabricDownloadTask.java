ab36c701549d4505159669c93464fe8a08883872
artdeell <ultramaksim@gmail.com>
2023-08-29 16:33:51 +0300

    Feat[fabric]: different API impl + GUI-less installer

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/FabricDownloadTask.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/FabricDownloadTask.java
@@ -6,67 +6,68 @@ import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 import net.kdt.pojavlaunch.utils.DownloadUtils;
+import net.kdt.pojavlaunch.value.launcherprofiles.LauncherProfiles;
+import net.kdt.pojavlaunch.value.launcherprofiles.MinecraftProfile;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 public class FabricDownloadTask implements Runnable, Tools.DownloaderFeedback{
-    private final File mDestinationDir;
-    private final File mDestinationFile;
     private final ModloaderDownloadListener mModloaderDownloadListener;
-
-    public FabricDownloadTask(ModloaderDownloadListener modloaderDownloadListener) {
+    private final String mGameVersion;
+    private final String mLoaderVersion;
+    private final boolean mCreateProfile;
+    public FabricDownloadTask(ModloaderDownloadListener modloaderDownloadListener, String mGameVersion, String mLoaderVersion, boolean mCreateProfile) {
         this.mModloaderDownloadListener = modloaderDownloadListener;
-        this.mDestinationDir = new File(Tools.DIR_CACHE, "fabric-installer");
-        this.mDestinationFile = new File(mDestinationDir, "fabric-installer.jar");
+        this.mGameVersion = mGameVersion;
+        this.mLoaderVersion = mLoaderVersion;
+        this.mCreateProfile = mCreateProfile;
     }
 
     @Override
     public void run() {
         ProgressKeeper.submitProgress(ProgressLayout.INSTALL_MODPACK, 0, R.string.fabric_dl_progress);
         try {
-            if(runCatching()) mModloaderDownloadListener.onDownloadFinished(mDestinationFile);
+            if(runCatching()) mModloaderDownloadListener.onDownloadFinished(null);
+            else mModloaderDownloadListener.onDataNotAvailable();
         }catch (IOException e) {
             mModloaderDownloadListener.onDownloadError(e);
         }
         ProgressLayout.clearProgress(ProgressLayout.INSTALL_MODPACK);
     }
 
-    private boolean runCatching() throws IOException {
-        if(!mDestinationDir.exists() && !mDestinationDir.mkdirs()) throw new IOException("Failed to create cache directory");
-        String[] urlAndVersion = FabricUtils.getInstallerUrlAndVersion();
-        if(urlAndVersion == null) {
-            mModloaderDownloadListener.onDataNotAvailable();
+    private boolean runCatching() throws IOException{
+        String fabricJson = DownloadUtils.downloadString(FabricUtils.createJsonDownloadUrl(mGameVersion, mLoaderVersion));
+        String versionId;
+        try {
+            JSONObject fabricJsonObject = new JSONObject(fabricJson);
+            versionId = fabricJsonObject.getString("id");
+        }catch (JSONException e) {
+            e.printStackTrace();
             return false;
         }
-        File versionFile = new File(mDestinationDir, "fabric-installer-version");
-        boolean shouldDownloadInstaller = true;
-        if(urlAndVersion[1] != null && versionFile.canRead()) { // if we know the latest version that we have and the server has
-            try {
-                shouldDownloadInstaller = !urlAndVersion[1].equals(Tools.read(versionFile.getAbsolutePath()));
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
+        File versionJsonDir = new File(Tools.DIR_HOME_VERSION, versionId);
+        File versionJsonFile = new File(versionJsonDir, versionId+".json");
+        if(versionJsonDir.isFile()) throw new IOException("Version destination directory is a file!");
+        if(!versionJsonDir.exists() && !versionJsonDir.mkdirs()) throw new IOException("Failed to create version directory");
+        Tools.write(versionJsonFile.getAbsolutePath(), fabricJson);
+        if(mCreateProfile) {
+            LauncherProfiles.load();
+            MinecraftProfile fabricProfile = new MinecraftProfile();
+            fabricProfile.lastVersionId = versionId;
+            fabricProfile.name = "Minecraft " + mGameVersion + " with Fabric " + mLoaderVersion;
+            Map<String, MinecraftProfile> profiles = LauncherProfiles.mainProfileJson.profiles;
+            String uuid = UUID.randomUUID().toString();
+            while (profiles.get(uuid) != null) uuid = UUID.randomUUID().toString();
+            profiles.put(uuid, fabricProfile);
+            LauncherProfiles.write();
         }
-        if(shouldDownloadInstaller) {
-            if (urlAndVersion[0] != null) {
-                byte[] buffer = new byte[8192];
-                DownloadUtils.downloadFileMonitored(urlAndVersion[0], mDestinationFile, buffer, this);
-                if(urlAndVersion[1] != null) {
-                    try {
-                        Tools.write(versionFile.getAbsolutePath(), urlAndVersion[1]);
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return true;
-            } else {
-                mModloaderDownloadListener.onDataNotAvailable();
-                return false;
-            }
-        }else{
-            return true;
-        }
+        return true;
     }
 
     @Override
