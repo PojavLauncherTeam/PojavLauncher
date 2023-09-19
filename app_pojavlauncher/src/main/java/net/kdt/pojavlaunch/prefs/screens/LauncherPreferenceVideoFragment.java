fa7930ef4581c86883a313da62fb5ed39f68baa3
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-09-18 23:12:04 -0300

    Update LauncherPreferenceVideoFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
@@ -19,19 +19,14 @@ import net.kdt.pojavlaunch.prefs.LauncherPreferences;
  * Fragment for any settings video related
  */
 public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment {
-    private CustomSeekBarPreference seek5;
-    private SwitchPreference sustainedPerfSwitch;
-    private ListPreference rendererListPreference;
-    private SwitchPreferenceCompat forceVSyncPreference;
-
     @Override
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_video);
 
-        // Desabilitar o comportamento de verificação de entalhe no Android 8.1 e abaixo.
+        //Disable notch checking behavior on android 8.1 and below.
         findPreference("ignoreNotch").setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && PREF_NOTCH_SIZE > 0);
 
-        seek5 = findPreference("resolutionRatio");
+        CustomSeekBarPreference seek5 = findPreference("resolutionRatio");
         seek5.setMin(25);
         seek5.setSuffix(" %");
 
@@ -40,35 +35,26 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
             seek5.setValue(100);
         }
 
-        // O desempenho sustentado só está disponível desde o Nougat
-        sustainedPerfSwitch = findPreference("sustainedPerformance");
+        // Sustained performance is only available since Nougat
+        SwitchPreference sustainedPerfSwitch = findPreference("sustainedPerformance");
         sustainedPerfSwitch.setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N);
 
-        rendererListPreference = findPreference("renderer");
+        ListPreference rendererListPreference = findPreference("renderer");
         Tools.RenderersList renderersList = Tools.getCompatibleRenderers(getContext());
         rendererListPreference.setEntries(renderersList.rendererDisplayNames);
         rendererListPreference.setEntryValues(renderersList.rendererIds.toArray(new String[0]));
 
-        forceVSyncPreference = findPreference("force_vsync");
-        forceVSyncPreference.setVisible(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
-
-        // Cache o valor de algumas preferências para evitar chamadas desnecessárias ao SharedPreferences
-        boolean useAlternateSurface = LauncherPreferences.get(getContext(), LauncherPreferences.PREF_USE_ALTERNATE_SURFACE, false);
-        computeVisibility(useAlternateSurface);
+        computeVisibility();
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences p, String s) {
         super.onSharedPreferenceChanged(p, s);
-
-        // Compute visibility again only if the alternate surface preference changed
-        if (s.equals(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE)) {
-            boolean useAlternateSurface = LauncherPreferences.get(getContext(), LauncherPreferences.PREF_USE_ALTERNATE_SURFACE, false);
-            computeVisibility(useAlternateSurface);
-        }
+        computeVisibility();
     }
 
-    private void computeVisibility(boolean useAlternateSurface) {
-        forceVSyncPreference.setVisible(useAlternateSurface);
+    private void computeVisibility(){
+        SwitchPreferenceCompat preference = findPreference("force_vsync");
+        preference.setVisible(LauncherPreferences.PREF_USE_ALTERNATE_SURFACE);
     }
 }
