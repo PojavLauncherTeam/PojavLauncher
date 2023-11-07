a18ad558b8ffce3e6daeec67344251c9392a7d4f
artdeell <ultramaksim@gmail.com>
2023-11-07 20:48:44 +0300

    Fix[mainactivity]: comment truncated

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
@@ -350,7 +350,7 @@ public class MainActivity extends BaseActivity implements ControlButtonMenuListe
         int requiredJavaVersion = 8;
         if(version.javaVersion != null) requiredJavaVersion = version.javaVersion.majorVersion;
         Tools.launchMinecraft(this, minecraftAccount, minecraftProfile, versionId, requiredJavaVersion);
-        //Note that we actually stall in the above function, even if the game
+        //Note that we actually stall in the above function, even if the game crashes. But let's be safe.
         Tools.runOnUiThread(()-> mServiceBinder.isActive = false);
     }
 
