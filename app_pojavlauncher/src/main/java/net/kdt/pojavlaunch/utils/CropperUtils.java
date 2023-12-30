256b12fa23d2c76b927bbff7e72320dee2215f86
artdeell <ultramaksim@gmail.com>
2023-12-30 16:31:51 +0300

    Feat[cropper]: Implement dual-path decoding and minimize lag when moving image quickly

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/CropperUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/CropperUtils.java
@@ -1,9 +1,12 @@
 package net.kdt.pojavlaunch.utils;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
 
@@ -13,6 +16,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import net.kdt.pojavlaunch.R;
+import net.kdt.pojavlaunch.imgcropper.BitmapCropBehaviour;
+import net.kdt.pojavlaunch.imgcropper.CropperBehaviour;
 import net.kdt.pojavlaunch.imgcropper.CropperView;
 import net.kdt.pojavlaunch.imgcropper.RegionDecoderCropBehaviour;
 
@@ -30,6 +35,7 @@ public class CropperUtils {
 
     private static void openCropperDialog(Context context, Uri selectedUri,
                                           final CropperListener cropperListener) {
+        ContentResolver contentResolver = context.getContentResolver();
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
                 .setTitle(R.string.cropper_title);
         builder.setView(R.layout.dialog_cropper);
@@ -38,23 +44,48 @@ public class CropperUtils {
         AlertDialog dialog = builder.show();
         CropperView cropImageView = dialog.findViewById(R.id.crop_dialog_view);
         assert cropImageView != null;
-        try (InputStream inputStream = context.getContentResolver().openInputStream(selectedUri)){
-            //BitmapCropBehaviour bitmapCropBehaviour = new BitmapCropBehaviour(cropImageView);
-            //bitmapCropBehaviour.loadBitmap(BitmapFactory.decodeStream(inputStream));
-            //cropImageView.cropperBehaviour = bitmapCropBehaviour;
-            RegionDecoderCropBehaviour cropBehaviour = new RegionDecoderCropBehaviour(cropImageView);
-            cropBehaviour.loadRegionDecoder(BitmapRegionDecoder.newInstance(inputStream, false));
-            cropImageView.cropperBehaviour = cropBehaviour;
-            cropImageView.requestLayout();
-            bindViews(dialog, cropImageView);
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v->{
-                dialog.dismiss();
-                cropperListener.onCropped(cropImageView.crop(256));
-            });
-        }catch (IOException e){
+        try {
+            try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
+                if(inputStream == null) return; // The provider has crashed, there is no point in trying again.
+                try {
+                    BitmapRegionDecoder regionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+                    RegionDecoderCropBehaviour cropBehaviour = new RegionDecoderCropBehaviour(cropImageView);
+                    cropBehaviour.loadRegionDecoder(regionDecoder);
+                    finishViewSetup(dialog, cropImageView, cropBehaviour, cropperListener);
+                    return;
+                }catch (IOException e) {
+                    // Catch IOE here to detect the case when BitmapRegionDecoder does not support this image format.
+                    // If it does not, we will just have to load the bitmap in full resolution using BitmapFactory.
+                    Log.w("CropperUtils", "Failed to load image into BitmapRegionDecoder", e);
+                }
+            }
+            // We can safely re-open the stream here as ACTION_OPEN_DOCUMENT grants us long-term access
+            // to the file that we have picked.
+            try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
+                if(inputStream == null) return;
+                Bitmap originalBitmap = BitmapFactory.decodeStream(inputStream);
+                BitmapCropBehaviour cropBehaviour = new BitmapCropBehaviour(cropImageView);
+                cropBehaviour.loadBitmap(originalBitmap);
+                finishViewSetup(dialog, cropImageView, cropBehaviour, cropperListener);
+            }
+        }catch (Exception e){
             cropperListener.onFailed(e);
             dialog.dismiss();
         }
+    }
+
+
+    private static void finishViewSetup(AlertDialog dialog,
+                                 CropperView cropImageView,
+                                 CropperBehaviour cropBehaviour,
+                                 CropperListener cropperListener) {
+        cropImageView.cropperBehaviour = cropBehaviour;
+        cropImageView.requestLayout();
+        bindViews(dialog, cropImageView);
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v->{
+            dialog.dismiss();
+            cropperListener.onCropped(cropImageView.crop(256));
+        });
     }
 
     private static void bindViews(AlertDialog alertDialog, CropperView imageCropperView) {
