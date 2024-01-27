bb16bffdf7f717df559af6710b11396a9d1d432d
artdeell <ultramaksim@gmail.com>
2024-01-27 11:52:22 +0300

    Feat[cropper]: asynchronously load the image and change output resolution

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/CropperUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/CropperUtils.java
@@ -15,7 +15,9 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import net.kdt.pojavlaunch.PojavApplication;
 import net.kdt.pojavlaunch.R;
+import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.imgcropper.BitmapCropBehaviour;
 import net.kdt.pojavlaunch.imgcropper.CropperBehaviour;
 import net.kdt.pojavlaunch.imgcropper.CropperView;
@@ -43,48 +45,62 @@ public class CropperUtils {
         builder.setNegativeButton(android.R.string.cancel, null);
         AlertDialog dialog = builder.show();
         CropperView cropImageView = dialog.findViewById(R.id.crop_dialog_view);
+        View finishProgressBar = dialog.findViewById(R.id.crop_dialog_progressbar);
         assert cropImageView != null;
-        try {
-            try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
-                if(inputStream == null) return; // The provider has crashed, there is no point in trying again.
-                try {
-                    BitmapRegionDecoder regionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
-                    RegionDecoderCropBehaviour cropBehaviour = new RegionDecoderCropBehaviour(cropImageView);
-                    cropBehaviour.loadRegionDecoder(regionDecoder);
-                    finishViewSetup(dialog, cropImageView, cropBehaviour, cropperListener);
-                    return;
-                }catch (IOException e) {
-                    // Catch IOE here to detect the case when BitmapRegionDecoder does not support this image format.
-                    // If it does not, we will just have to load the bitmap in full resolution using BitmapFactory.
-                    Log.w("CropperUtils", "Failed to load image into BitmapRegionDecoder", e);
-                }
-            }
-            // We can safely re-open the stream here as ACTION_OPEN_DOCUMENT grants us long-term access
-            // to the file that we have picked.
-            try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
-                if(inputStream == null) return;
-                Bitmap originalBitmap = BitmapFactory.decodeStream(inputStream);
-                BitmapCropBehaviour cropBehaviour = new BitmapCropBehaviour(cropImageView);
-                cropBehaviour.loadBitmap(originalBitmap);
-                finishViewSetup(dialog, cropImageView, cropBehaviour, cropperListener);
-            }
-        }catch (Exception e){
-            cropperListener.onFailed(e);
+        assert finishProgressBar != null;
+        bindViews(dialog, cropImageView);
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v->{
             dialog.dismiss();
+            // I chose 70 dp here because it resolves to 192x192 on my device
+            // (which has a typical screen density of 395 dpi)
+            cropperListener.onCropped(cropImageView.crop((int) Tools.dpToPx(70)));
+        });
+        PojavApplication.sExecutorService.execute(()->{
+            try {
+                loadBehaviour(cropImageView, contentResolver, selectedUri);
+                Tools.runOnUiThread(()->finishProgressBar.setVisibility(View.GONE));
+            }catch (Exception e){ Tools.runOnUiThread(()->{
+                cropperListener.onFailed(e);
+                dialog.dismiss();
+            });}
+        });
+
+    }
+
+
+    private static void loadBehaviour(CropperView cropImageView,
+                                      ContentResolver contentResolver,
+                                      Uri selectedUri) throws Exception {
+        try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
+            if(inputStream == null) return; // The provider has crashed, there is no point in trying again.
+            try {
+                BitmapRegionDecoder regionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+                RegionDecoderCropBehaviour cropBehaviour = new RegionDecoderCropBehaviour(cropImageView);
+                cropBehaviour.loadRegionDecoder(regionDecoder);
+                finishViewSetup(cropImageView, cropBehaviour);
+                return;
+            }catch (IOException e) {
+                // Catch IOE here to detect the case when BitmapRegionDecoder does not support this image format.
+                // If it does not, we will just have to load the bitmap in full resolution using BitmapFactory.
+                Log.w("CropperUtils", "Failed to load image into BitmapRegionDecoder", e);
+            }
+        }
+        // We can safely re-open the stream here as ACTION_OPEN_DOCUMENT grants us long-term access
+        // to the file that we have picked.
+        try (InputStream inputStream = contentResolver.openInputStream(selectedUri)) {
+            if(inputStream == null) return;
+            Bitmap originalBitmap = BitmapFactory.decodeStream(inputStream);
+            BitmapCropBehaviour cropBehaviour = new BitmapCropBehaviour(cropImageView);
+            cropBehaviour.loadBitmap(originalBitmap);
+            finishViewSetup(cropImageView,cropBehaviour);
         }
     }
 
 
-    private static void finishViewSetup(AlertDialog dialog,
-                                 CropperView cropImageView,
-                                 CropperBehaviour cropBehaviour,
-                                 CropperListener cropperListener) {
-        cropImageView.cropperBehaviour = cropBehaviour;
-        cropImageView.requestLayout();
-        bindViews(dialog, cropImageView);
-        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v->{
-            dialog.dismiss();
-            cropperListener.onCropped(cropImageView.crop(256));
+    private static void finishViewSetup(CropperView cropImageView, CropperBehaviour cropBehaviour) {
+        Tools.runOnUiThread(()->{
+            cropImageView.setCropperBehaviour(cropBehaviour);
+            cropImageView.requestLayout();
         });
     }
 
@@ -102,12 +118,12 @@ public class CropperUtils {
                 imageCropperView.verticalLock = verticalLock.isChecked()
         );
         reset.setOnClickListener(v->
-            imageCropperView.cropperBehaviour.resetTransforms()
+            imageCropperView.resetTransforms()
         );
     }
 
     @SuppressWarnings("unchecked")
-    public static void startCropper(ActivityResultLauncher<?> resultLauncher, Context context) {
+    public static void startCropper(ActivityResultLauncher<?> resultLauncher) {
         ActivityResultLauncher<String[]> realResultLauncher =
                 (ActivityResultLauncher<String[]>) resultLauncher;
         realResultLauncher.launch(new String[]{"image/*"});
