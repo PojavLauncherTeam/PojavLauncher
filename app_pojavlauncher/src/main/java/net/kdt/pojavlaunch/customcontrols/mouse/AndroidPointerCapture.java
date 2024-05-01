b154ec310577c3dddcb5282cf932eb5357af2092
artdeell <ultramaksim@gmail.com>
2024-05-01 13:37:48 +0300

    Fix[input]: implement scrolling gestures for touchpads

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/AndroidPointerCapture.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/AndroidPointerCapture.java
@@ -15,10 +15,12 @@ import org.lwjgl.glfw.CallbackBridge;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class AndroidPointerCapture implements ViewTreeObserver.OnWindowFocusChangeListener, View.OnCapturedPointerListener {
+    private static final float TOUCHPAD_SCROLL_THRESHOLD = 1;
     private final AbstractTouchpad mTouchpad;
     private final View mHostView;
     private final float mScaleFactor;
-    private final float mPointerPrescale = Tools.dpToPx(1);
+    private final float mMousePrescale = Tools.dpToPx(1);
+    private final Scroller mScroller = new Scroller(TOUCHPAD_SCROLL_THRESHOLD);
 
     public AndroidPointerCapture(AbstractTouchpad touchpad, View hostView, float scaleFactor) {
         this.mScaleFactor = scaleFactor;
@@ -47,13 +49,26 @@ public class AndroidPointerCapture implements ViewTreeObserver.OnWindowFocusChan
 
     @Override
     public boolean onCapturedPointer(View view, MotionEvent event) {
+        // Yes, we actually not only receive relative mouse events here, but also absolute touchpad ones!
+        // Read from relative axis directly to work around.
+        float relX = event.getAxisValue(MotionEvent.AXIS_RELATIVE_X);
+        float relY = event.getAxisValue(MotionEvent.AXIS_RELATIVE_Y);
         if(!CallbackBridge.isGrabbing()) {
             enableTouchpadIfNecessary();
-            mTouchpad.applyMotionVector(event.getX() * mPointerPrescale, event.getY() * mPointerPrescale);
+            // Yes, if the user's touchpad is multi-touch we will also receive events for that.
+            // So, handle the scrolling gesture ourselves.
+            relX *= mMousePrescale;
+            relY *= mMousePrescale;
+            if(event.getPointerCount() < 2) {
+                mTouchpad.applyMotionVector(relX, relY);
+                mScroller.resetScrollOvershoot();
+            } else {
+                mScroller.performScroll(relX, relY);
+            }
         } else {
             // Position is updated by many events, hence it is send regardless of the event value
-            CallbackBridge.mouseX += (event.getX() * mScaleFactor);
-            CallbackBridge.mouseY += (event.getY() * mScaleFactor);
+            CallbackBridge.mouseX += (relX * mScaleFactor);
+            CallbackBridge.mouseY += (relY * mScaleFactor);
             CallbackBridge.sendCursorPos(CallbackBridge.mouseX, CallbackBridge.mouseY);
         }
 
