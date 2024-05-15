1c52bf2a59270bca3b2401c0aed136bce3c33f73
Mathias-Boulay <mathiasboulay@free.fr>
2024-05-16 00:14:34 +0200

    cleanup: remove unused code
    Signed-off-by: Mathias-Boulay <mathiasboulay@free.fr>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/SingleTapConfirm.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/SingleTapConfirm.java
@@ -8,12 +8,6 @@ import androidx.annotation.NonNull;
 public class SingleTapConfirm extends SimpleOnGestureListener {
 	@Override
 	public boolean onSingleTapUp(MotionEvent event) {
-		System.out.println("single tap up !");
 		return true;
-	}
-
-	@Override
-	public boolean onDoubleTap(@NonNull MotionEvent e) {
-		return super.onDoubleTap(e);
 	}
 }
