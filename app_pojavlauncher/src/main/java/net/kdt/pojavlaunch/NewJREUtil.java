74492eaa7ea7f367f824339cb642c83c30bc3b02
SolDev <40839581+SolDev69@users.noreply.github.com>
2024-06-25 12:42:26 -0400

    fix(typo):  annoying typo in translation string names (#5666)
    removed 'r' in "compartible"

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/NewJREUtil.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/NewJREUtil.java
@@ -97,7 +97,7 @@ public class NewJREUtil {
 
     private static void showRuntimeFail(Activity activity, JMinecraftVersionList.Version verInfo) {
         Tools.dialogOnUiThread(activity, activity.getString(R.string.global_error),
-                activity.getString(R.string.multirt_nocompartiblert, verInfo.javaVersion.majorVersion));
+                activity.getString(R.string.multirt_nocompatiblert, verInfo.javaVersion.majorVersion));
     }
 
     private enum InternalRuntime {
