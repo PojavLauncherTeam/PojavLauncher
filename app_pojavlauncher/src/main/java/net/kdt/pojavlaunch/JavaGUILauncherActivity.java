3b1ad643427a21fea527e7252764f3c94d97c973
Maksim Belov <ultramaksim@gmail.com>
2024-07-17 00:10:17 +0300

    Feat[gui_launcher]: refactor java selection and modify argument processing

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JavaGUILauncherActivity.java
@@ -15,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 
 import com.kdt.LoggerView;
 
@@ -185,6 +186,7 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
         try {
             File cacheFile = new File(getCacheDir(), "mod-installer-temp");
             InputStream contentStream = getContentResolver().openInputStream(uri);
+            if(contentStream == null) throw new IOException("Failed to open content stream");
             try (FileOutputStream fileOutputStream = new FileOutputStream(cacheFile)) {
                 IOUtils.copy(contentStream, fileOutputStream);
             }
@@ -195,23 +197,69 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
         }
     }
 
+    public Runtime selectRuntime(File modFile) {
+        int javaVersion = getJavaVersion(modFile);
+        if(javaVersion == -1) {
+            finalErrorDialog(getString(R.string.execute_jar_failed_to_read_file));
+            return null;
+        }
+        String nearestRuntime = MultiRTUtils.getNearestJreName(javaVersion);
+        if(nearestRuntime == null) {
+            finalErrorDialog(getString(R.string.multirt_nocompatiblert, javaVersion));
+            return null;
+        }
+        Runtime selectedRuntime = MultiRTUtils.forceReread(nearestRuntime);
+        int selectedJavaVersion = Math.max(javaVersion, selectedRuntime.javaVersion);
+        if(selectedJavaVersion > 17) {
+            finalErrorDialog(getString(R.string.execute_jar_incompatible_runtime, selectedJavaVersion));
+        }
+        return selectedRuntime;
+    }
+
+    private File findModPath(List<String> argList) {
+        int argsSize = argList.size();
+        for(int i = 0; i < argsSize; i++) {
+            // Look for the -jar argument
+            if(!argList.get(i).equals("-jar")) continue;
+            int pathIndex = i+1;
+            // Check if the supposed path is out of the argument bounds
+            if(pathIndex >= argsSize) return null;
+            // Use the path as a file
+            return new File(argList.get(pathIndex));
+        }
+        return null;
+    }
+
     private void startModInstaller(File modFile, String javaArgs) {
         new Thread(() -> {
-            Runtime runtime = pickJreForMod(modFile);
-            launchJavaRuntime(runtime, modFile, javaArgs);
+            // Maybe replace with more advanced arg parsing logic later
+            List<String> argList = Arrays.asList(javaArgs.split(" "));
+            File selectedMod = modFile;
+            if(selectedMod == null) {
+                // If modFile is not specified directly, try to extract the -jar argument from the javaArgs
+                selectedMod = findModPath(argList);
+            }
+            Runtime selectedRuntime;
+            if(selectedMod == null) {
+                // We were unable to find out the path to the mod. In that case, use the default runtime.
+                selectedRuntime = MultiRTUtils.forceReread(LauncherPreferences.PREF_DEFAULT_RUNTIME);
+            }else {
+                // Autoselect it properly in the other case.
+                selectedRuntime = selectRuntime(selectedMod);
+                // If the selection failed, just return. The autoselect function has already shown the dialog.
+                if(selectedRuntime == null) return;
+            }
+            launchJavaRuntime(selectedRuntime, modFile, argList);
         }, "JREMainThread").start();
     }
 
-    private Runtime pickJreForMod(File modFile) {
-        String jreName = LauncherPreferences.PREF_DEFAULT_RUNTIME;
-        if(modFile != null) {
-            int javaVersion = getJavaVersion(modFile);
-            if(javaVersion != -1) {
-                String autoselectRuntime = MultiRTUtils.getNearestJreName(javaVersion);
-                if (autoselectRuntime != null) jreName = autoselectRuntime;
-            }
-        }
-        return MultiRTUtils.forceReread(jreName);
+    private void finalErrorDialog(CharSequence msg) {
+        runOnUiThread(()-> new AlertDialog.Builder(this)
+                .setTitle(R.string.global_error)
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.ok, (d,w)->this.finish())
+                .setCancelable(false)
+                .show());
     }
 
     @Override
@@ -301,21 +349,19 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
                 Toast.LENGTH_SHORT).show();
     }
 
-    public void launchJavaRuntime(Runtime runtime, File modFile, String javaArgs) {
+    public void launchJavaRuntime(Runtime runtime, File modFile, List<String> javaArgs) {
         JREUtils.redirectAndPrintJRELog();
         try {
             List<String> javaArgList = new ArrayList<>();
 
             // Enable Caciocavallo
             Tools.getCacioJavaArgs(javaArgList,runtime.javaVersion == 8);
-            
-            if (javaArgs != null) {
-                javaArgList.addAll(Arrays.asList(javaArgs.split(" ")));
-            } else {
+
+            javaArgList.addAll(javaArgs);
+            if(modFile != null) {
                 javaArgList.add("-jar");
                 javaArgList.add(modFile.getAbsolutePath());
             }
-
             
             if (LauncherPreferences.PREF_JAVA_SANDBOX) {
                 Collections.reverse(javaArgList);
@@ -351,30 +397,36 @@ public class JavaGUILauncherActivity extends BaseActivity implements View.OnTouc
     public int getJavaVersion(File modFile) {
         try (ZipFile zipFile = new ZipFile(modFile)){
             ZipEntry manifest = zipFile.getEntry("META-INF/MANIFEST.MF");
+            Log.i("JavaVersion", "Mf entry: "+manifest);
             if(manifest == null) return -1;
+            Log.i("JavaVersion", "Found manifest entry");
 
             String manifestString = Tools.read(zipFile.getInputStream(manifest));
             String mainClass = Tools.extractUntilCharacter(manifestString, "Main-Class:", '\n');
             if(mainClass == null) return -1;
+            Log.i("JavaVersion", "Found main class: "+mainClass);
 
             mainClass = mainClass.trim().replace('.', '/') + ".class";
             ZipEntry mainClassFile = zipFile.getEntry(mainClass);
             if(mainClassFile == null) return -1;
+            Log.i("JavaVersion", "Found main class entry: "+mainClass);
 
             InputStream classStream = zipFile.getInputStream(mainClassFile);
             byte[] bytesWeNeed = new byte[8];
             int readCount = classStream.read(bytesWeNeed);
             classStream.close();
             if(readCount < 8) return -1;
+            Log.i("JavaVersion", "File is long enough!");
 
             ByteBuffer byteBuffer = ByteBuffer.wrap(bytesWeNeed);
             if(byteBuffer.getInt() != 0xCAFEBABE) return -1;
+            Log.i("JavaVersion", "File is Java class!");
             short minorVersion = byteBuffer.getShort();
             short majorVersion = byteBuffer.getShort();
             Log.i("JavaGUILauncher", majorVersion+","+minorVersion);
             return classVersionToJavaVersion(majorVersion);
         }catch (Exception e) {
-            e.printStackTrace();
+            Log.e("JavaVersion", "Exception thrown", e);
             return -1;
         }
     }
