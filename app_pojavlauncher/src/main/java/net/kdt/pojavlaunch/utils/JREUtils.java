52b33c00d3aa5ffed8fe0084ac6cccac409cc55f
Vera-Firefly <1964762970@qq.com>
2024-08-05 12:52:56 +0800

    Fix Snapdragon launch with Zink renderer 1.17 + forge loader crash

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -210,7 +210,6 @@ public class JREUtils {
         envMap.put("allow_higher_compat_version", "true");
         envMap.put("allow_glsl_extension_directive_midshader", "true");
         envMap.put("MESA_LOADER_DRIVER_OVERRIDE", "zink");
-        envMap.put("VTEST_SOCKET_NAME", new File(Tools.DIR_CACHE, ".virgl_test").getAbsolutePath());
 
         envMap.put("LD_LIBRARY_PATH", LD_LIBRARY_PATH);
         envMap.put("PATH", jreHome + "/bin:" + Os.getenv("PATH"));
