37c15d64b419fe5122601a2c2ff4f415bcdb4df2
artdeell <ultramaksim@gmail.com>
2025-01-13 23:24:03 +0300

    Fix[ltw]: fix actions, add forgotten file

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -1386,13 +1386,15 @@ public final class Tools {
         // Currently, only 32-bit x86 does not have the Zink binary
         boolean deviceHasZinkBinary = !(Architecture.is32BitsDevice() && Architecture.isx86Device());
         boolean deviceHasOpenGLES3 = JREUtils.getDetectedVersion() >= 3;
+        // LTW is an optional proprietary dependency
+        boolean appHasLtw = new File(Tools.NATIVE_LIB_DIR, "libltw.so").exists();
         List<String> rendererIds = new ArrayList<>(defaultRenderers.length);
         List<String> rendererNames = new ArrayList<>(defaultRendererNames.length);
         for(int i = 0; i < defaultRenderers.length; i++) {
             String rendererId = defaultRenderers[i];
             if(rendererId.contains("vulkan") && !deviceHasVulkan) continue;
             if(rendererId.contains("zink") && !deviceHasZinkBinary) continue;
-            if(rendererId.contains("ltw") && !deviceHasOpenGLES3) continue;
+            if(rendererId.contains("ltw") && (!deviceHasOpenGLES3 || !appHasLtw)) continue;
             rendererIds.add(rendererId);
             rendererNames.add(defaultRendererNames[i]);
         }
