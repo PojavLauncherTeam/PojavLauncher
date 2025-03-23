3351bc6f9840d9b159736d6c0f1126a70e48aa6f
alexytomi <60690056+alexytomi@users.noreply.github.com>
2025-03-24 01:55:43 +0800

    [Renderer/MG] Add MobileGlues renderer
    Thank you @Tungstend and @Swung0x48 for the help!
    
    Co-authored-by: Tungstend <hanjinlong197@gmail.com>
    Co-authored-by: Swung0x48 <swung0x48@outlook.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -466,6 +466,7 @@ public class JREUtils {
             case "opengles3":
                 renderLibrary = "libgl4es_114.so"; break;
             case "vulkan_zink": renderLibrary = "libOSMesa.so"; break;
+            case "opengles_mobileglues": renderLibrary = "libmobileglues.so"; break; // TODO: Add aarch64 detection because MG only supports aarch64
             case "opengles3_ltw" : renderLibrary = "libltw.so"; break;
             default:
                 Log.w("RENDER_LIBRARY", "No renderer selected, defaulting to opengles2");
