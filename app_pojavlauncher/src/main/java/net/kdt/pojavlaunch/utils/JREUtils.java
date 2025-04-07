a472c9836edcdbe975532b74379604c4d5421fcd
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-04-07 06:02:55 +0000

    fix execute jar final

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -229,10 +229,7 @@ public class JREUtils {
             if(LOCAL_RENDERER.equals("opengles3_ltw")) {
                 envMap.put("LIBGL_ES", "3");
                 envMap.put("POJAVEXEC_EGL","libltw.so"); // Use ANGLE EGL
-            }
-        }
-
-        if (LOCAL_RENDERER.equals("opengles3_mges")) {
+            } else if(LOCAL_RENDERER.equals("opengles3_mges")) {
             envMap.put("LIBGL_ES", "3");
             envMap.put("POJAVEXEC_EGL","libmobileglues.so");
             envMap.put("MG_DIR_PATH", Tools.DIR_CACHE.getAbsolutePath());
@@ -243,6 +240,7 @@ public class JREUtils {
             envMap.put("MG_enableExtGL43", MG_EXT_GL43);
             envMap.put("MG_enableExtComputeShader", MG_EXT_CS);
         }
+    }
 
         if(LauncherPreferences.PREF_BIG_CORE_AFFINITY) envMap.put("POJAV_BIG_CORE_AFFINITY", "1");
         envMap.put("AWTSTUB_WIDTH", Integer.toString(CallbackBridge.windowWidth > 0 ? CallbackBridge.windowWidth : CallbackBridge.physicalWidth));
